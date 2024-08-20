6fc16917d390ea6d7f29d418c33a587a4abfab04
voorhs <ilya_aleksev_2016@list.ru>
2024-08-20 17:37:02 +0300

    add base scoring and prediction for multilabel

--- src/modules/scoring/linear.py
+++ src/modules/scoring/linear.py
@@ -1,4 +1,5 @@
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.multioutput import ClassifierChain
 
 from .base import DataHandler, ScoringModule
 
@@ -18,12 +19,21 @@ class LinearScorer(ScoringModule):
     ```
     """
 
+    def __init__(self, multilabel=False):
+        self.multilabel = multilabel
+
     def fit(self, data_handler: DataHandler):
         collection = data_handler.get_best_collection()
         dataset = collection.get(include=["embeddings", "metadatas"])
         features = dataset["embeddings"]
         labels = [dct["intent_id"] for dct in dataset["metadatas"]]
-        clf = LogisticRegressionCV(cv=3, n_jobs=8)
+
+        if self.multilabel:
+            base_clf = LogisticRegression()
+            clf = ClassifierChain(base_clf, order="random", random_state=0)
+        else:
+            clf = LogisticRegressionCV(cv=3, n_jobs=8)
+
         clf.fit(features, labels)
 
         self._clf = clf
@@ -32,9 +42,9 @@ class LinearScorer(ScoringModule):
     def predict(self, utterances: list[str]):
         features = self._emb_func(utterances)
         return self._clf.predict_proba(features)
-    
+
     def clear_cache(self):
         model = self._emb_func._model
-        model.to(device='cpu')
+        model.to(device="cpu")
         del model
         self.collection = None
