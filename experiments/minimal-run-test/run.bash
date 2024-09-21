c49b24a16e9d793e5574ec1437b707276984a4b0
voorhs <ilya_alekseev_2016@list.ru>
2024-09-21 15:22:45 +0300

    first attempt of adding github actions

--- experiments/minimal-run-test/run.bash
+++ experiments/minimal-run-test/run.bash
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Run the first command
 poetry run autointent \
     --config-path experiments/minimal-run-test/configs/multiclass.yaml \
     --multiclass-path experiments/minimal-run-test/data/clinc_subset.json \
@@ -7,6 +10,16 @@ poetry run autointent \
     --verbose \
     --run-name multiclass-cpu
 
+# Capture the exit code of the first command
+exit_code_1=$?
+echo "Exit code of the first command: $exit_code_1"
+
+if [ $exit_code_1 -ne 0 ]; then
+    echo "First command failed with exit code $exit_code_1"
+    exit $exit_code_1
+fi
+
+# Run the second command
 poetry run autointent \
     --config-path experiments/minimal-run-test/configs/multilabel.yaml \
     --multiclass-path experiments/minimal-run-test/data/clinc_subset.json \
@@ -15,3 +28,12 @@ poetry run autointent \
     --device "cpu" \
     --verbose \
     --run-name multilabel-cpu
+
+# Capture the exit code of the second command
+exit_code_2=$?
+echo "Exit code of the second command: $exit_code_2"
+
+if [ $exit_code_2 -ne 0 ]; then
+    echo "Second command failed with exit code $exit_code_2"
+    exit $exit_code_2
+fi
