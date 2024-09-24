666e38215f7dae76d90e1bd1dbb17efd477905ee
voorhs <ilya_alekseev_2016@list.ru>
2024-09-24 15:08:13 +0300

    refactor threshold predictor and enhance tunable predictor

--- autointent/modules/prediction/threshold.py
+++ autointent/modules/prediction/threshold.py
@@ -13,6 +13,10 @@ class ThresholdPredictor(PredictionModule):
         self.multilabel = context.multilabel
         self.tags = context.data_handler.tags
 
+        if isinstance(self.thresh, list):
+            assert len(self.thresh) == context.n_classes
+            self.thresh = np.array(self.thresh)
+
         if not context.data_handler.has_oos_samples():
             logger = logging.getLogger(__name__)
             logger.warning(
@@ -21,12 +25,39 @@ class ThresholdPredictor(PredictionModule):
             )
 
     def predict(self, scores: list[list[float]]):
-        if not self.multilabel:
-            pred_classes = np.argmax(scores, axis=1)
-            best_scores = scores[np.arange(len(scores)), pred_classes]
-            pred_classes[best_scores < self.thresh] = -1  # out of scope
-        else:
-            pred_classes = (scores >= self.thresh).astype(int)
-            if self.tags:
-                pred_classes = apply_tags(pred_classes, scores, self.tags)
-        return pred_classes
+        if self.multilabel:
+            return multilabel_predict(scores, self.thresh, self.tags)
+        return multiclass_predict(scores, self.thresh)
+
+
+def multiclass_predict(scores: list[list[float]], thresh: float | np.ndarray):
+    """
+    Return
+    ---
+    array of int labels, shape (n_samples,)
+    """
+    pred_classes = np.argmax(scores, axis=1)
+    best_scores = scores[np.arange(len(scores)), pred_classes]
+    
+    if isinstance(thresh, float):
+        pred_classes[best_scores < thresh] = -1  # out of scope
+    else:
+        thresh_selected = thresh[pred_classes]
+        pred_classes[best_scores < thresh_selected] = -1  # out of scope
+
+    return pred_classes
+
+
+def multilabel_predict(scores: list[list[float]], thresh: float | np.ndarray, tags):
+    """
+    Return
+    ---
+    array of binary labels, shape (n_samples, n_classes)
+    """
+    if isinstance(thresh, float):
+        res = (scores >= thresh).astype(int)
+    else:
+        res = (scores >= thresh[None, :]).astype(int)
+    if tags:
+        res = apply_tags(res, scores, tags)
+    return res
