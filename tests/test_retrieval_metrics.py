8faf3625c2f152aa1ab0e0b23049e97a266e8182
Roman Solomatin <samoed.roman@gmail.com>
2024-09-26 12:31:16 +0300

    refactor tests (#7)
    Co-authored-by: voorhs <ilya_alekseev_2016@list.ru>

--- tests/test_retrieval_metrics.py
+++ tests/test_retrieval_metrics.py
@@ -1,310 +1,90 @@
-def test_map():
-    from autointent.metrics.retrieval import retrieval_map
-    import numpy as np
+from autointent.metrics.retrieval import (
+    retrieval_map,
+    retrieval_hit_rate,
+    retrieval_precision,
+    retrieval_ndcg,
+    retrieval_mrr,
+)
+import numpy as np
+import pytest
 
-    # case 1
-    query_labels = [1]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-    ground_truth = 7/12
+
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([1], [[2, 1, 1]], None, 7 / 12),
+        ([3], [[3, 1, 1]], None, 1.0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], None, 19 / 24),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], 2, 0.75),
+        ([3], [[2, 1, 1]], None, 0.0),
+        ([3], [[2, 1, 1]], 2, 0.0),
+        ([3, 1], [[2, 1, 1], [2, 4, 4]], None, 0.0),
+    ],
+)
+def test_map(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_map(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
-    # case 2
-    query_labels = [3]
-    candidates_labels = [
-        [3,1,1]
-    ]
-    k = None
-    ground_truth = 1.
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
 
-    # case 3
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = None
-    ground_truth = 19/24
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = 2
-    ground_truth = 0.75
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 5
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-    ground_truth = 0.
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 6
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = 2
-    ground_truth = 0.
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 7
-    query_labels = [3,1]
-    candidates_labels = [
-        [2,1,1],
-        [2,4,4],
-    ]
-    k = None
-    ground_truth = 0.
-    output = retrieval_map(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-def test_hit_rate():
-    from autointent.metrics.retrieval import retrieval_hit_rate
-    import numpy as np
-
-    # case 1
-    query_labels = [1]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    ground_truth = 1.
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([1], [[2, 1, 1]], None, 1.0),
+        ([3], [[2, 1, 1]], None, 0.0),
+        ([3], [[2, 1, 1]], 2, 0.0),
+        ([3, 1], [[2, 1, 1], [3, 1, 1]], 1, 0.0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], None, 1.0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], 2, 1.0),
+    ],
+)
+def test_hit_rate(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_hit_rate(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
-    # case 2
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
 
-    ground_truth = 0.
-    output = retrieval_hit_rate(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = None
-
-    ground_truth = 1.
-    output = retrieval_hit_rate(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = 2
-
-    ground_truth = 1.
-    output = retrieval_hit_rate(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 5
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = 1
-
-    ground_truth = 0.5
-    output = retrieval_hit_rate(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-def test_precision():
-    from autointent.metrics.retrieval import retrieval_precision
-    import numpy as np
-
-    # case 1
-    query_labels = [1]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    ground_truth = 2/3
-    output = retrieval_precision(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    ground_truth = 0.
-    output = retrieval_precision(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = None
-
-    ground_truth = 0.5
-    output = retrieval_precision(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = 2
-
-    ground_truth = 0.5
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([1], [[2, 1, 1]], None, 2 / 3),
+        ([3], [[2, 1, 1]], None, 0.0),
+        ([3], [[2, 1, 1]], 2, 0.0),
+        ([3, 1], [[2, 1, 1], [3, 1, 1]], 1, 0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], None, 0.5),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], 2, 0.5),
+    ],
+)
+def test_precision(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_precision(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_ndcg():
-    from autointent.metrics.retrieval import retrieval_ndcg
-    import numpy as np
-
-    # case 1
-    query_labels = [1]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    dcg = 1 / np.log2(3) + 1 / np.log2(4)
-    idcg = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth = dcg / idcg
-    output = retrieval_ndcg(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    dcg = 0
-    idcg = 0
-    ground_truth = 0
-    output = retrieval_ndcg(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = None
-
-    dcg_1 = 1 / np.log2(3) + 1 / np.log2(4)
-    idcg_1 = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth_1 = dcg_1 / idcg_1
-    dcg_2 = 1 / np.log2(2)
-    idcg_2 = 1 / np.log2(2)
-    ground_truth_2 = dcg_2 / idcg_2
-
-    ground_truth = 0.5 * (ground_truth_1 + ground_truth_2)
-
-    output = retrieval_ndcg(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = 2
-
-    dcg_1 = 1 / np.log2(3)
-    idcg_1 = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth_1 = dcg_1 / idcg_1
-    dcg_2 = 1 / np.log2(2)
-    idcg_2 = 1 / np.log2(2)
-    ground_truth_2 = dcg_2 / idcg_2
-
-    ground_truth = 0.5 * (ground_truth_1 + ground_truth_2)
-
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([1], [[2, 1, 1]], None, 0.6934264036172708),
+        ([3], [[2, 1, 1]], None, 0.0),
+        ([3], [[2, 1, 1]], 2, 0.0),
+        ([3, 1], [[2, 1, 1], [3, 1, 1]], 1, 0.0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], None, 0.8467132018086354),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], 2, 0.6934264036172708),
+    ],
+)
+def test_ndcg(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_ndcg(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_mrr():
-    from autointent.metrics.retrieval import retrieval_mrr
-    import numpy as np
-
-    # case 1
-    query_labels = [1]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    ground_truth = 0.5
-    output = retrieval_mrr(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [3]
-    candidates_labels = [
-        [2,1,1],
-    ]
-    k = None
-
-    ground_truth = 0.
-    output = retrieval_mrr(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,1,1],
-        [3,1,1]
-    ]
-    k = None
-
-    ground_truth = 0.75
-    output = retrieval_mrr(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,2,1],
-        [3,1,1]
-    ]
-    k = 2
-
-    ground_truth = 0.5
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([1], [[2, 1, 1]], None, 0.5),
+        ([3], [[2, 1, 1]], None, 0.0),
+        ([3], [[2, 1, 1]], 2, 0.0),
+        ([3, 1], [[2, 1, 1], [3, 1, 1]], 1, 0.0),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], None, 0.75),
+        ([1, 3], [[2, 1, 1], [3, 1, 1]], 2, 0.75),
+    ],
+)
+def test_mrr(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_mrr(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
--- tests/test_retrieval_metrics_intersecting.py
+++ tests/test_retrieval_metrics_intersecting.py
@@ -1,348 +1,123 @@
-def test_map():
-    from autointent.metrics.retrieval import retrieval_map_intersecting
-    import numpy as np
+import numpy as np
+import pytest
 
-    # case 1
-    query_labels = [[0,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-    ground_truth = 7/12
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
+from autointent.metrics.retrieval import (
+    retrieval_hit_rate_intersecting,
+    retrieval_map_intersecting,
+    retrieval_mrr_intersecting,
+    retrieval_ndcg_intersecting,
+    retrieval_precision_intersecting,
+)
 
-    # case 1b
-    query_labels = [[1,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-    ground_truth = 7/12
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
 
-    # case 2
-    query_labels = [[0,0,1]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]]
-    ]
-    k = None
-    ground_truth = 1.
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2b
-    query_labels = [[1,0,1]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]]
-    ]
-    k = None
-    ground_truth = 1.
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [[0,1,0],[0,0,1]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-        [[0,0,1],[0,1,0],[0,1,0]]
-    ]
-    k = None
-    ground_truth = 19/24
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [[0,1,0],[0,0,1]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-        [[0,0,1],[0,1,0],[0,1,0]]
-    ]
-    k = 2
-    ground_truth = 0.75
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 5
-    query_labels = [[1,0,1]]
-    candidates_labels = [
-        [[0,0,0],[0,1,0],[0,1,0]]
-    ]
-    k = None
-    ground_truth = 0.
-    output = retrieval_map_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 6
-    query_labels = [[1,0,1]]
-    candidates_labels = [
-        [[0,0,0],[0,1,0],[0,1,0]]
-    ]
-    k = 2
-    ground_truth = 0.
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([[0, 1, 0]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 7 / 12),
+        ([[1, 1, 0]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 7 / 12),
+        ([[0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[1, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[0, 1, 0], [0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 19 / 24),
+        ([[0, 1, 0], [0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 0], [0, 1, 0]]], 2, 0.75),
+        ([[1, 0, 1]], [[[0, 0, 0], [0, 1, 0], [0, 1, 0]]], None, 0.0),
+        ([[1, 0, 1]], [[[0, 0, 0], [0, 1, 0], [0, 1, 0]]], 2, 0.0),
+    ],
+)
+def test_map(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_map_intersecting(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_hit_rate():
-    from autointent.metrics.retrieval import retrieval_hit_rate_intersecting
-    import numpy as np
-
-    # case 1
-    query_labels = [[0,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-
-    ground_truth = 1.
-    output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 1b
-    query_labels = [[1,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-
-    ground_truth = 1.
-    output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [[1,0,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]]
-    ]
-    k = None
-
-    ground_truth = 0.
-    output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [[0,0,1],[0,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-        [[0,1,0],[0,1,0],[0,1,0]],
-    ]
-    k = None
-
-    ground_truth = 1.
-    output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [[0,0,1],[0,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-        [[0,1,0],[0,1,0],[0,1,0]],
-    ]
-    k = 2
-
-    ground_truth = 1.
-    output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 5
-    query_labels = [[0,1,0],[0,1,0]]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-        [[0,1,0],[0,1,0],[0,1,0]],
-    ]
-    k = 1
-
-    ground_truth = 0.5
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([[0, 1, 0]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[1, 1, 0]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[1, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[0, 1, 0], [0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1.0),
+        ([[0, 1, 0], [0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 0], [0, 1, 0]]], 2, 1.0),
+        ([[1, 0, 1]], [[[0, 0, 0], [0, 1, 0], [0, 1, 0]]], None, 0.0),
+        ([[1, 0, 1]], [[[0, 0, 0], [0, 1, 0], [0, 1, 0]]], 2, 0.0),
+    ],
+)
+def test_hit_rate(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_hit_rate_intersecting(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_precision():
-    from autointent.metrics.retrieval import retrieval_precision_intersecting
-    import numpy as np
-
-    # case 1
-    query_labels = [[0,0,1],]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-
-    ground_truth = 1/3
-    output = retrieval_precision_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 1b
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[0,0,1],[0,1,0],[0,1,0]],
-    ]
-    k = None
-
-    ground_truth = 1.
-    output = retrieval_precision_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[1,0,0],[1,0,0]],
-    ]
-    k = None
-
-    ground_truth = 0.
-    output = retrieval_precision_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [[1,0,1],[0,1,1]]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[0,0,1]],
-        [[1,0,0],[1,0,0],[0,0,1]],
-    ]
-    k = None
-
-    ground_truth = 0.5
-    output = retrieval_precision_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [[1,0,1],[0,1,1]]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[0,0,1]],
-        [[1,0,0],[1,0,0],[0,0,1]],
-    ]
-    k = 2
-
-    ground_truth = 0.25
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([[0, 0, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1 / 3),
+        ([[0, 1, 1]], [[[0, 0, 1], [0, 1, 0], [0, 1, 0]]], None, 1),
+        ([[0, 1, 1]], [[[1, 0, 0], [1, 0, 0], [1, 0, 0]]], None, 0),
+        ([[1, 0, 1], [0, 1, 1]], [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1, 0, 0], [1, 0, 0], [0, 0, 1]]], None, 0.5),
+        ([[1, 0, 1], [0, 1, 1]], [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1, 0, 0], [1, 0, 0], [0, 0, 1]]], 2, 0.25),
+    ],
+)
+def test_precision(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_precision_intersecting(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_ndcg():
-    from autointent.metrics.retrieval import retrieval_ndcg_intersecting
-    import numpy as np
-
-    # case 1
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[1,0,1]],
-    ]
-    k = None
-
-    dcg = 1 / np.log2(3) + 1 / np.log2(4)
-    idcg = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth = dcg / idcg
-    output = retrieval_ndcg_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[1,0,0],[1,0,0]],
-    ]
-    k = None
-
-    dcg = 0
-    idcg = 0
-    ground_truth = 0
-    output = retrieval_ndcg_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [[0,1,1],[0,1,1]]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[0,0,1]],
-        [[0,1,0],[1,0,0],[1,0,0]],
-    ]
-    k = None
-
-    dcg_1 = 1 / np.log2(3) + 1 / np.log2(4)
-    idcg_1 = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth_1 = dcg_1 / idcg_1
-    dcg_2 = 1 / np.log2(2)
-    idcg_2 = 1 / np.log2(2)
-    ground_truth_2 = dcg_2 / idcg_2
-
-    ground_truth = 0.5 * (ground_truth_1 + ground_truth_2)
-
-    output = retrieval_ndcg_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [[0,1,1],[0,1,1]]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[0,0,1]],
-        [[0,1,0],[1,0,0],[1,0,0]],
-    ]
-    k = 2
-
-    dcg_1 = 1 / np.log2(3)
-    idcg_1 = 1 / np.log2(2) + 1 / np.log2(3)
-    ground_truth_1 = dcg_1 / idcg_1
-    dcg_2 = 1 / np.log2(2)
-    idcg_2 = 1 / np.log2(2)
-    ground_truth_2 = dcg_2 / idcg_2
-
-    ground_truth = 0.5 * (ground_truth_1 + ground_truth_2)
-
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        (
+            [[0, 1, 1]],
+            [[[1, 0, 0], [0, 1, 0], [1, 0, 1]]],
+            None,
+            0.6934264036172708,
+        ),
+        ([[0, 1, 1]], [[[1, 0, 0], [1, 0, 0], [1, 0, 0]]], None, 0),
+        (
+            [[0, 1, 1], [0, 1, 1]],
+            [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[0, 1, 0], [1, 0, 0], [1, 0, 0]]],
+            None,
+            0.8467132018086354,
+        ),
+        (
+            [[0, 1, 1], [0, 1, 1]],
+            [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[0, 1, 0], [1, 0, 0], [1, 0, 0]]],
+            2,
+            0.6934264036172708,
+        ),
+    ],
+)
+def test_ndcg(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_ndcg_intersecting(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
 
 
-def test_mrr():
-    from autointent.metrics.retrieval import retrieval_mrr_intersecting
-    import numpy as np
-
-    # case 1
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[0,1,0],[1,0,1]],
-    ]
-    k = None
-
-    ground_truth = 0.5
-    output = retrieval_mrr_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 2
-    query_labels = [[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[1,0,0],[1,0,0]],
-    ]
-    k = None
-
-    ground_truth = 0.
-    output = retrieval_mrr_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 3
-    query_labels = [[0,1,1],[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[1,1,0],[1,0,1]],
-        [[1,0,1],[1,1,0],[1,0,1]],
-    ]
-    k = None
-
-    ground_truth = 0.75
-    output = retrieval_mrr_intersecting(query_labels, candidates_labels, k)
-    np.testing.assert_almost_equal(output, ground_truth)
-
-    # case 4
-    query_labels = [1,3]
-    candidates_labels = [
-        [2,2,1],
-        [3,1,1]
-    ]
-    query_labels = [[0,0,1],[0,1,1],]
-    candidates_labels = [
-        [[1,0,0],[1,1,0],[1,0,1]],
-        [[1,0,1],[1,1,0],[1,0,1]],
-    ]
-    k = 2
-
-    ground_truth = 0.5
+@pytest.mark.parametrize(
+    "query_labels, candidates_labels, k, ground_truth",
+    [
+        ([[0, 1, 1]], [[[1, 0, 0], [0, 1, 0], [1, 0, 1]]], None, 0.5),
+        ([[0, 1, 1]], [[[1, 0, 0], [1, 0, 0], [1, 0, 0]]], None, 0.0),
+        (
+            [[0, 1, 1], [0, 1, 1]],
+            [
+                [[1, 0, 0], [1, 1, 0], [1, 0, 1]],
+                [[1, 0, 1], [1, 1, 0], [1, 0, 1]],
+            ],
+            None,
+            0.75,
+        ),
+        (
+            [
+                [0, 0, 1],
+                [0, 1, 1],
+            ],
+            [
+                [[1, 0, 0], [1, 1, 0], [1, 0, 1]],
+                [[1, 0, 1], [1, 1, 0], [1, 0, 1]],
+            ],
+            2,
+            0.5,
+        ),
+    ],
+)
+def test_mrr(query_labels, candidates_labels, k, ground_truth):
     output = retrieval_mrr_intersecting(query_labels, candidates_labels, k)
     np.testing.assert_almost_equal(output, ground_truth)
