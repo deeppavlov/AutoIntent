eb91f998fad26f1d95e556cd3fb2e80e00f14589
voorhs <ilya_alekseev_2016@list.ru>
2024-09-26 13:11:07 +0300

    add tests for multilabel scoring metrics

--- tests/test_scoring_metrics.py
+++ tests/test_scoring_metrics.py
@@ -1,4 +1,4 @@
-from autointent.metrics.scoring import scoring_roc_auc, scoring_log_likelihood
+from autointent.metrics.scoring import scoring_roc_auc, scoring_log_likelihood, scoring_hit_rate, scoring_neg_coverage
 import numpy as np
 import pytest
 
@@ -19,6 +19,18 @@ import pytest
                 ]
             ),
         ),
+        (
+            [[1, 0, 0, 0]],
+            [[0.1, 0.3, 0.5, 0.1]],
+            np.mean(
+                [
+                    np.log(0.1),
+                    np.log(1-0.3),
+                    np.log(1-0.5),
+                    np.log(1-0.1)
+                ]
+            ),
+        )
     ],
 )
 def test_neg_cross_entropy(labels, scores, ground_truth):
@@ -63,4 +75,143 @@ def test_neg_cross_entropy(labels, scores, ground_truth):
 )
 def test_roc_auc(labels, scores, ground_truth):
     output = scoring_roc_auc(labels, scores)
+    np.testing.assert_almost_equal(output, ground_truth)
+
+
+@pytest.mark.parametrize(
+    ["labels", "scores", "ground_truth"],
+    [
+        (
+            [
+                [1, 0, 0, 0],
+                [0, 1, 0, 0,],
+                [0, 0, 1, 0,],
+                [0, 0, 0, 1,],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.2],
+                [0.1, 0.5, 0.3, 0.2],
+                [0.1, 0.3, 0.5, 0.2],
+                [0.2, 0.3, 0.1, 0.5],
+            ],
+            1.0,
+        ),
+        (
+            [
+                [1, 0, 0, 0],
+                [0, 1, 0, 0,],
+                [0, 0, 1, 0,],
+                [0, 0, 0, 1,],
+            ],
+            [
+                [0.5, 0.3, 0.1, 0.2],
+                [0.1, 0.5, 0.3, 0.2],
+                [0.1, 0.5, 0.3, 0.2],
+                [0.2, 0.3, 0.1, 0.5],
+            ],
+            0.75,
+        ),
+        (
+            [
+                [1, 0, 0, 0],
+                [0, 1, 0, 0,],
+                [0, 1, 1, 0,],
+                [0, 0, 0, 1,],
+            ],
+            [
+                [0.5, 0.3, 0.1, 0.2],
+                [0.1, 0.5, 0.3, 0.2],
+                [0.1, 0.5, 0.3, 0.2],
+                [0.1, 0.3, 0.2, 0.5],
+            ],
+            1,
+        ),
+        (
+            [
+                [1, 0, 0, 0],
+            ],
+            [
+                [0.5, 0.3, 0.1, 0.2],
+            ],
+            1,
+        ),
+        (
+            [
+                [0, 1, 0, 0],
+            ],
+            [
+                [0.5, 0.3, 0.1, 0.2],
+            ],
+            0,
+        ),
+    ]
+)
+def test_hit_rate(labels, scores, ground_truth):
+    output = scoring_hit_rate(labels, scores)
+    np.testing.assert_almost_equal(output, ground_truth)
+
+
+@pytest.mark.parametrize(
+    ["labels", "scores", "ground_truth"],
+    [
+        (
+            [
+                [1, 0, 0, 0],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            1,
+        ),
+        (
+            [
+                [1, 1, 0, 0],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            2/3,
+        ),
+        (
+            [
+                [0, 1, 0, 0],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            2/3,
+        ),
+        (
+            [
+                [0, 0, 0, 1],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            0,
+        ),
+        (
+            [
+                [1, 1, 1, 1],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            0,
+        ),
+        (
+            [
+                [0, 1, 0, 0],
+                [0, 0, 0, 1],
+            ],
+            [
+                [0.5, 0.3, 0.2, 0.1],
+                [0.5, 0.3, 0.2, 0.1],
+            ],
+            1/3,
+        ),
+    ]
+)
+def test_coverage(labels, scores, ground_truth):
+    output = scoring_neg_coverage(labels, scores)
     np.testing.assert_almost_equal(output, ground_truth)
