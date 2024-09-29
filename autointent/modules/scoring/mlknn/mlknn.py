be1123cb1d9be3554d5bd5bd8f27b5368641f02f
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2024-09-29 19:33:19 +0300

    add mlknn

---
+++ autointent/modules/scoring/mlknn/mlknn.py
@@ -0,0 +1,111 @@
+from typing import Any
+
+import numpy as np
+from chromadb.types import Collection
+from numpy.typing import NDArray
+from sklearn.preprocessing import OneHotEncoder
+
+from autointent.modules.scoring.base import Context, ScoringModule
+
+
+class MLKnnScorer(ScoringModule):
+    _multilabel: bool
+    _collection: Collection
+    _n_classes: int
+    _converter: callable
+    encoder: OneHotEncoder
+    _prior_prob_true: NDArray[np.float64]
+    _prior_prob_false: NDArray[np.float64]
+    _cond_prob_true: NDArray[np.float64]
+    _cond_prob_false: NDArray[np.float64]
+
+    def __init__(self, k: int, s: float = 1.0, ignore_first_neighbours: int = 0):
+        self.k = k
+        self.s = s
+        self.ignore_first_neighbours = ignore_first_neighbours
+
+    def fit(self, context: Context) -> None:
+        self._multilabel = context.multilabel
+        self._collection = context.get_best_collection()
+        self._n_classes = context.n_classes
+        self._converter = context.vector_index.metadata_as_labels
+
+        self.encoder = OneHotEncoder(sparse_output=False)
+        train_labels = context.data_handler.labels_train
+        labels = self.encoder.fit_transform(np.array(train_labels).reshape(-1, 1))
+        self._prior_prob_true, self._prior_prob_false = self._compute_prior(labels)
+        self._cond_prob_true, self._cond_prob_false = self._compute_cond(
+            np.array(context.data_handler.utterances_train), labels
+        )
+
+    def _compute_prior(self, y: NDArray[np.float64]) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+        prior_prob_true = (self.s + y.sum(axis=0)) / (self.s * 2 + y.shape[0])
+        prior_prob_false = 1 - prior_prob_true
+        return prior_prob_true, prior_prob_false
+
+    def _compute_cond(
+        self, x: NDArray[np.str_], y: NDArray[np.float64]
+    ) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+        c = np.zeros((self._n_classes, self.k + 1), dtype=int)
+        cn = np.zeros((self._n_classes, self.k + 1), dtype=int)
+
+        neighbors = self._get_neighbors(x)
+
+        for instance in range(x.shape[0]):
+            deltas = y[neighbors[instance]].sum(axis=0).astype(int)
+            for label in range(self._n_classes):
+                if y[instance, label] == 1:
+                    c[label, deltas[label]] += 1
+                else:
+                    cn[label, deltas[label]] += 1
+
+        c_sum = c.sum(axis=1)
+        cn_sum = cn.sum(axis=1)
+
+        cond_prob_true = (self.s + c) / (self.s * (self.k + 1) + c_sum[:, None])
+        cond_prob_false = (self.s + cn) / (self.s * (self.k + 1) + cn_sum[:, None])
+
+        return cond_prob_true, cond_prob_false
+
+    def _get_neighbors(self, x: NDArray[np.str_]) -> NDArray[np.int64]:
+        query_res = self._collection.query(
+            query_texts=x, n_results=self.k + self.ignore_first_neighbours, include=["metadatas"]
+        )
+        return np.array(
+            [self._converter(candidates[self.ignore_first_neighbours :]) for candidates in query_res["metadatas"]]
+        )
+
+    def predict(self, x: NDArray[np.str_]) -> NDArray[np.int64]:
+        result = np.zeros((len(x), self._n_classes), dtype=int)
+        neighbors = self._get_neighbors(x)
+
+        for instance in range(len(x)):
+            deltas = np.sum(self.encoder.transform(np.array(neighbors[instance]).reshape(-1, 1)), axis=0).astype(int)
+
+            for label in range(self._n_classes):
+                p_true = self._prior_prob_true[label] * self._cond_prob_true[label, deltas[label]]
+                p_false = self._prior_prob_false[label] * self._cond_prob_false[label, deltas[label]]
+                result[instance, label] = int(p_true >= p_false)
+
+        return result
+
+    def predict_proba(self, x: list[str]) -> NDArray[np.float64]:
+        result = np.zeros((len(x), self._n_classes), dtype=float)
+        np_x = np.array(x)
+        neighbors = self._get_neighbors(np_x)
+
+        for instance in range(np_x.shape[0]):
+            deltas = np.sum(self.encoder.transform(np.array(neighbors[instance]).reshape(-1, 1)), axis=0).astype(int)
+
+            for label in range(self._n_classes):
+                p_true = self._prior_prob_true[label] * self._cond_prob_true[label, deltas[label]]
+                p_false = self._prior_prob_false[label] * self._cond_prob_false[label, deltas[label]]
+                result[instance, label] = p_true / (p_true + p_false)
+
+        return result
+
+    def clear_cache(self) -> None:
+        model = self._collection._embedding_function._model  # noqa: SLF001
+        model.to(device="cpu")
+        del model
+        self._collection = None
