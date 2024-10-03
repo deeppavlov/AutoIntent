04d1152ae59e67d9fd14ccd89411f854a29dc2ef
voorhs <ilya_alekseev_2016@list.ru>
2024-10-03 23:52:47 +0300

    minor addition

--- autointent/modules/__init__.py
+++ autointent/modules/__init__.py
@@ -10,32 +10,32 @@ from .regexp import RegExp
 from .retrieval import RetrievalModule, VectorDBModule
 from .scoring import DNNCScorer, KNNScorer, LinearScorer, MLKnnScorer, ScoringModule
 
-RETRIEVAL_MODULES_MULTICLASS = {
+RETRIEVAL_MODULES_MULTICLASS: dict[str, RetrievalModule] = {
     "vector_db": VectorDBModule,
 }
 
 RETRIEVAL_MODULES_MULTILABEL = RETRIEVAL_MODULES_MULTICLASS
 
-SCORING_MODULES_MULTICLASS = {
+SCORING_MODULES_MULTICLASS: dict[str, ScoringModule] = {
     "dnnc": DNNCScorer,
     "knn": KNNScorer,
     "linear": LinearScorer,
 }
 
-SCORING_MODULES_MULTILABEL = {
+SCORING_MODULES_MULTILABEL: dict[str, ScoringModule] = {
     "knn": KNNScorer,
     "linear": LinearScorer,
     "mlknn": MLKnnScorer,
 }
 
-PREDICTION_MODULES_MULTICLASS = {
+PREDICTION_MODULES_MULTICLASS: dict[str, PredictionModule] = {
     "argmax": ArgmaxPredictor,
     "jinoos": JinoosPredictor,
     "threshold": ThresholdPredictor,
     "tunable": TunablePredictor,
 }
 
-PREDICTION_MODULES_MULTILABEL = {
+PREDICTION_MODULES_MULTILABEL: dict[str, PredictionModule] = {
     "threshold": ThresholdPredictor,
     "tunable": TunablePredictor,
 }
