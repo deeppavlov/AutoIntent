9f23916743fe51a8aff4799442bd225f71159cdf
voorhs <ilya_alekseev_2016@list.ru>
2024-10-14 23:57:34 +0300

    move from metadata to direct labels storing

--- autointent/context/vector_index.py
+++ autointent/context/vector_index.py
@@ -1,6 +1,4 @@
 import logging
-from collections.abc import Callable
-from functools import partial
 from typing import Any
 
 import faiss
@@ -12,26 +10,25 @@ from .data_handler import DataHandler
 
 
 class VectorIndex:
-    def __init__(self, model_name: str, device: str, converter: Callable) -> None:
+    def __init__(self, model_name: str, device: str) -> None:
         self.model_name = model_name
         self.device = device
         self.embedding_model = SentenceTransformer(model_name, device=device)
         self.index = None
-        self.metadata: list[dict] = []
-        self.converter = converter
+        self.labels: list[int] | list[list[int]] = []    # (n_samples,) or (n_samples, n_classes)
 
-    def add(self, texts: list[str], metadata: list[dict]) -> None:
+    def add(self, texts: list[str], labels: list[int] | list[list[int]]) -> None:
         self.texts = texts
         embeddings = self.embed(texts)
         if self.index is None:
             self.index = faiss.IndexFlatIP(embeddings.shape[1])
         self.index.add(embeddings)  # type: ignore # noqa: PGH003
-        self.metadata.extend(metadata)
+        self.labels.extend(labels)
 
     def delete(self) -> None:
         if self.index:
             self.index.reset()
-        self.metadata = []
+        self.labels = []
 
     def _search_by_text(self, texts: list[str], k: int) -> list[list[dict]]:
         query_embedding = self.embedding_model.encode(texts)
@@ -48,19 +45,16 @@ class VectorIndex:
         for inds, dists in zip(indices, distances, strict=True):
             cur_res = []
             for ind, dist in zip(inds, dists, strict=True):
-                cur_res.append({"id": ind, "distance": dist, "metadata": self.metadata[ind]})
+                cur_res.append({"id": ind, "distance": dist, "label": self.labels[ind]})
             results.append(cur_res)
 
         return results
-
-    def get_metadata(self) -> list[dict]:
-        return self.metadata
 
     def get_all_embeddings(self) -> npt.NDArray[Any]:
         return self.index.reconstruct_n(0, self.index.ntotal)
 
     def get_all_labels(self) -> list[int] | list[list[int]]:
-        return self.converter(self.metadata)
+        return self.labels
 
     def query(
         self, queries: list[str] | list[npt.NDArray], k: int
@@ -83,13 +77,11 @@ class VectorIndex:
         func = self._search_by_text if isinstance(queries[0], str) else self._search_by_embedding
         all_results = func(queries, k)
 
-        all_metadata = [[result["metadata"] for result in results] for results in all_results]
+        all_labels = [[self.labels[result["id"]] for result in results] for results in all_results]
         all_distances = [[result["distance"] for result in results] for results in all_results]
         all_texts = [[self.texts[result["id"]] for result in results] for results in all_results]
 
-        labels = [self.converter(candidates) for candidates in all_metadata] if self.converter else all_metadata
-
-        return labels, all_distances, all_texts
+        return all_labels, all_distances, all_texts
 
     def embed(self, utterances: list[str]) -> npt.NDArray[np.float32]:
         return self.embedding_model.encode(utterances, convert_to_numpy=True)
@@ -115,9 +107,8 @@ class VectorIndexClient:
     def create_index(self, model_name: str, data_handler: DataHandler) -> VectorIndex:
         self._logger.info("Creating index for model: %s", model_name)
 
-        index = VectorIndex(model_name, self.device, self.build_converter())
-        metadata = self.labels_as_metadata(data_handler.labels_train)
-        index.add(data_handler.utterances_train, metadata)
+        index = VectorIndex(model_name, self.device)
+        index.add(data_handler.utterances_train, data_handler.labels_train)
 
         self.indexes[model_name] = index
 
@@ -129,31 +120,5 @@ class VectorIndexClient:
             self.indexes[model_name].delete()
             del self.indexes[model_name]
 
-    def build_converter(self) -> Callable:
-        if self.multilabel:
-            return partial(_multilabel_metadata_as_labels, n_classes=self.n_classes)
-        return _multiclass_metadata_as_labels
-
-    def labels_as_metadata(self, labels: list[Any]) -> list[dict]:
-        if self.multilabel:
-            return _multilabel_labels_as_metadata(labels)
-        return _multiclass_labels_as_metadata(labels)
-
     def get_index(self, model_name: str) -> VectorIndex:
         return self.indexes[model_name]
-
-
-def _multiclass_labels_as_metadata(labels_list: list[int]) -> list[dict[str, Any]]:
-    return [{"intent_id": lab} for lab in labels_list]
-
-
-def _multilabel_labels_as_metadata(labels_list: list[list[int]]) -> list[dict[str, Any]]:
-    return [{str(i): lab for i, lab in enumerate(labs)} for labs in labels_list]
-
-
-def _multiclass_metadata_as_labels(metadata: list[dict[str, Any]]) -> list[int]:
-    return [dct["intent_id"] for dct in metadata]
-
-
-def _multilabel_metadata_as_labels(metadata: list[dict], n_classes: int) -> list[list[int]]:
-    return [[dct[str(i)] for i in range(n_classes)] for dct in metadata]
