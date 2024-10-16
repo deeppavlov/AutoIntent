10f88269bf3a958291d6bd78e52a2d0ea72010d4
James Raskind <raskind-korvin@yandex.ru>
2024-10-16 15:00:00 +0300

    add_logit_adaptivness

--- autointent/modules/__init__.py
+++ autointent/modules/__init__.py
@@ -1,5 +1,60 @@
 from .base import Module
-from .retrieval import RetrievalModule, VectorDBModule
-from .scoring import ScoringModule, KNNScorer, LinearScorer, DNNCScorer
-from .prediction import PredictionModule, ThresholdPredictor, ArgmaxPredictor, JinoosPredictor, TunablePredictor
+from .prediction import (
+    ArgmaxPredictor,
+    JinoosPredictor,
+    PredictionModule,
+    ThresholdPredictor,
+    TunablePredictor,
+    LogitAdaptivnessPredictor
+)
 from .regexp import RegExp
+from .retrieval import RetrievalModule, VectorDBModule
+from .scoring import DNNCScorer, KNNScorer, LinearScorer, MLKnnScorer, ScoringModule
+
+RETRIEVAL_MODULES_MULTICLASS: dict[str, type[RetrievalModule]] = {
+    "vector_db": VectorDBModule,
+}
+
+RETRIEVAL_MODULES_MULTILABEL = RETRIEVAL_MODULES_MULTICLASS
+
+SCORING_MODULES_MULTICLASS: dict[str, type[ScoringModule]] = {
+    "dnnc": DNNCScorer,
+    "knn": KNNScorer,
+    "linear": LinearScorer,
+}
+
+SCORING_MODULES_MULTILABEL: dict[str, type[ScoringModule]] = {
+    "knn": KNNScorer,
+    "linear": LinearScorer,
+    "mlknn": MLKnnScorer,
+}
+
+PREDICTION_MODULES_MULTICLASS: dict[str, type[PredictionModule]] = {
+    "argmax": ArgmaxPredictor,
+    "jinoos": JinoosPredictor,
+    "threshold": ThresholdPredictor,
+    "tunable": TunablePredictor,
+    "logit_adapt": LogitAdaptivnessPredictor 
+}
+
+PREDICTION_MODULES_MULTILABEL: dict[str, type[PredictionModule]] = {
+    "threshold": ThresholdPredictor,
+    "tunable": TunablePredictor,
+}
+__all__ = [
+    "Module",
+    "ArgmaxPredictor",
+    "JinoosPredictor",
+    "PredictionModule",
+    "ThresholdPredictor",
+    "TunablePredictor",
+    "LogitAdaptivnessPredictor"
+    "RegExp",
+    "RetrievalModule",
+    "VectorDBModule",
+    "DNNCScorer",
+    "KNNScorer",
+    "LinearScorer",
+    "MLKnnScorer",
+    "ScoringModule",
+]
--- autointent/modules/prediction/__init__.py
+++ autointent/modules/prediction/__init__.py
@@ -1,5 +1,8 @@
-from .base import PredictionModule
-from .threshold import ThresholdPredictor
 from .argmax import ArgmaxPredictor
-from .tunable import TunablePredictor
+from .base import PredictionModule
 from .jinoos import JinoosPredictor
+from .threshold import ThresholdPredictor
+from .tunable import TunablePredictor
+from .logit_adaptivness import LogitAdaptivnessPredictor
+
+__all__ = ["ArgmaxPredictor", "JinoosPredictor", "PredictionModule", "ThresholdPredictor", "TunablePredictor", "LogitAdaptivnessPredictor"]
