d9b708cb316cf460ce72fd34c9ecbcbf2b0a3b28
Darina Rustamova <darisha6602@gmail.com>
2024-10-23 00:17:01 +0300

    feat: added description_generation

---
+++ autointent/context/data_handler/description_generation.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+
+from autointent.context.data_handler.schemas import Dataset, Intent, Utterance
+
+PROMPT_DESCRIPTION = """
+{intent_name}
+{utterances}
+"""
+
+
+def get_utternaces_by_id(intents: list[Intent], utterances: list[Utterance]) -> dict[int, list[str]]:
+    pass
+
+
+def generate_intent_description(client: OpenAI, intent_name: str, utterances: list[str], model_name: str) -> str:
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": PROMPT_DESCRIPTION.format(intent_name=intent_name, utterances="\n".join(utterances)),
+            }
+        ],
+        model=model_name,
+    )
+    return chat_completion.choices[0].text.strip()
+
+
+def enhance_dataset_with_descriptions(
+    dataset: Dataset, api_base: str, api_key: str, model_name: str = "gpt-3.5-turbo"
+) -> Dataset:
+    client = OpenAI(
+        api_base=api_base,
+        api_key=api_key,
+    )
+    intent_utterances = get_utternaces_by_id(dataset.intents, dataset.utterances)
+
+    for intent in dataset.intents:
+        if not intent.description:
+            intent.description = generate_intent_description(
+                client=client, intent_name=intent.name, utterances=intent_utterances[intent.id], model_name=model_name
+            )
+    return dataset
