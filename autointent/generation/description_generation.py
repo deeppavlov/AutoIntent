ca4a1337c6eac5606d8a9510f6c016be4bc3068f
Darina Rustamova <darisha6602@gmail.com>
2024-10-23 16:32:29 +0300

    refactor: create prompt config

--- autointent/generation/description_generation.py
+++ autointent/generation/description_generation.py
@@ -4,52 +4,7 @@ from collections import defaultdict
 from openai import AsyncOpenAI
 
 from autointent.context.data_handler.schemas import Dataset, Intent, Utterance
-
-PROMPT_DESCRIPTION = """
-Your task is to write a description of the intent.
-
-You are given the name of the intent and user intentions related to it. The description should be:
-1) in declarative form
-2) no more than one sentence
-3) in the language in which the utterances and the name are written.
-
-Remember:
-Respond with just the description, no extra details.
-Keep in mind that either the names or user queries may not be provided.
-
-For example:
-
-name:
-activate_my_card
-user utterances:
-Please help me with my card. It won't activate.
-I tried but am unable to activate my card.
-I want to start using my card.
-description:
-user wants to activate his card
-
-name:
-beneficiary_not_allowed
-user utterances:
-
-description:
-user want to know why his beneficiary is not allowed
-
-name:
-оформление_отпуска
-user utterances:
-как оформить отпуск
-в какие даты надо оформить отпуск
-как запланировать отпуск
-description:
-пользователь спрашивает про оформление отпуска
-
-name:
-{intent_name}
-user utterances:
-{user_utterances}
-description:
-"""
+from autointent.generation.prompts import PROMPT_DESCRIPTION
 
 
 def get_utternaces_by_id(utterances: list[Utterance]) -> dict[int, list[str]]:
