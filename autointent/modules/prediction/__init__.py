14f5f193b3ff43188c45c74a5edb689c06accf93
JakeRaskind <55792828+JakeRaskind@users.noreply.github.com>
2024-10-30 18:50:35 +0300

    Update __init__.py

--- autointent/modules/prediction/__init__.py
+++ autointent/modules/prediction/__init__.py
@@ -1,9 +1,8 @@
 from .argmax import ArgmaxPredictor
 from .base import PredictionModule
 from .jinoos import JinoosPredictor
+from .logit_adaptivness import LogitAdaptivnessPredictor
 from .threshold import ThresholdPredictor
 from .tunable import TunablePredictor
-from .logit_adaptivness import LogitAdaptivnessPredictor
 
 __all__ = ["ArgmaxPredictor", "JinoosPredictor", "PredictionModule", "ThresholdPredictor", "TunablePredictor", "LogitAdaptivnessPredictor"]
-
