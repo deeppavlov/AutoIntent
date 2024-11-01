58bd6f3e392993c2ed37f5a5296d8d11a8a66e64
JakeRaskind <55792828+JakeRaskind@users.noreply.github.com>
2024-11-01 12:01:22 +0300

    Update logit_adaptivness.py

--- autointent/modules/prediction/logit_adaptivness.py
+++ autointent/modules/prediction/logit_adaptivness.py
@@ -7,7 +7,7 @@ from .base import Context, PredictionModule, get_prediction_evaluation_data
 
 
 class LogitAdaptivnessPredictor(PredictionModule):
-    default_search_space = ClassVar([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    default_search_space: ClassVar[list[float]] = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
     def __init__(self, search_space: list[float] | None = None) -> None:
         self.search_space = search_space if search_space is not None else self.default_search_space
