140a4ec83ea686f40d3ef2af29d8087815fa2b32
Darina Rustamova <darisha6602@gmail.com>
2024-11-07 19:22:41 +0300

    test: added inference pipeline cli

--- tests/pipeline/test_inference.py
+++ tests/pipeline/test_inference.py
@@ -4,12 +4,14 @@ from typing import Literal
 
 import pytest
 
+from autointent.configs.inference_cli import InferenceConfig
 from autointent.configs.optimization_cli import (
     EmbedderConfig,
     LoggingConfig,
     VectorIndexConfig,
 )
 from autointent.pipeline.inference import InferencePipeline
+from autointent.pipeline.inference.cli_endpoint import main as inference_pipeline
 from autointent.pipeline.optimization import PipelineOptimizer
 from autointent.pipeline.optimization.utils import load_config
 from tests.conftest import setup_environment
@@ -77,3 +79,29 @@ def test_inference_context(dataset, task_type):
         assert prediction.shape == (2,)
 
     context.dump()
+
+
+@pytest.mark.parametrize(
+    "task_type",
+    ["multiclass", "multilabel", "description"],
+)
+def test_inference_pipeline_cli(dataset, task_type):
+    db_dir, dump_dir, logs_dir = setup_environment()
+    search_space = get_search_space(task_type)
+
+    pipeline_optimizer = PipelineOptimizer.from_dict_config(search_space)
+
+    pipeline_optimizer.set_config(LoggingConfig(dirpath=Path(logs_dir).resolve(), dump_dir=dump_dir, dump_modules=True))
+    pipeline_optimizer.set_config(VectorIndexConfig(db_dir=Path(db_dir).resolve(), device="cuda", save_db=True))
+    pipeline_optimizer.set_config(EmbedderConfig(batch_size=16, max_length=32))
+    context = pipeline_optimizer.optimize_from_dataset(dataset, force_multilabel=(task_type == "multilabel"))
+
+    context.dump()
+
+    config = InferenceConfig(
+        data_path=ires.files("tests.assets.data").joinpath("clinc_subset.json"),
+        source_dir=dump_dir,
+        output_path=dump_dir,
+        log_level="CRITICAL",
+    )
+    inference_pipeline(config)
