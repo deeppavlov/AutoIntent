9545bbfc73924bc55a61ea1e54821b572df8a408
voorhs <ilya_alekseev_2016@list.ru>
2024-11-12 10:46:17 +0300

    stupid bug fix

--- autointent/context/vector_index_client/cache.py
+++ autointent/context/vector_index_client/cache.py
@@ -3,6 +3,6 @@ from uuid import uuid4
 
 
 def get_db_dir(db_dir: str | Path | None = None) -> Path:
-    db_dir = Path.cwd() / "vector_db_" + str(uuid4()) if db_dir is None else Path(db_dir)
+    db_dir = Path.cwd() / ("vector_db_" + str(uuid4())) if db_dir is None else Path(db_dir)
     db_dir.mkdir(parents=True, exist_ok=True)
     return db_dir
