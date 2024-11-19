1d73257ed0e859ff5da1edf76170d000cf83a126
voorhs <ilya_alekseev_2016@list.ru>
2024-11-19 12:02:01 +0300

    optimize threshold test

--- tests/modules/prediction/test_treshold.py
+++ tests/modules/prediction/test_treshold.py
@@ -1,11 +1,15 @@
 import numpy as np
+import pytest
 
 from autointent.context.data_handler import DataHandler
 from autointent.modules import KNNScorer, ThresholdPredictor
 from tests.conftest import setup_environment
 
 
-def get_fit_data(db_dir, dataset):
+@pytest.fixture
+def fit_data(dataset):
+    db_dir, dump_dir, logs_dir = setup_environment()
+
     data_handler = DataHandler(dataset)
 
     knn_params = {
@@ -22,31 +26,25 @@ def get_fit_data(db_dir, dataset):
     return scores, labels
 
 
-def test_predict_returns_correct_indices(dataset):
-    db_dir, dump_dir, logs_dir = setup_environment()
-
+def test_predict_returns_correct_indices(fit_data):
     predictor = ThresholdPredictor(0.5)
-    predictor.fit(*get_fit_data(db_dir, dataset))
+    predictor.fit(*fit_data)
     scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
     predictions = predictor.predict(scores)
     np.testing.assert_array_equal(predictions, np.array([1, 0, 1]))
 
 
-def test_predict_returns_list(dataset):
-    db_dir, dump_dir, logs_dir = setup_environment()
-
+def test_predict_returns_list(fit_data):
     predictor = ThresholdPredictor(np.array([0.5, 0.5, 0.5]), n_classes=3)
-    predictor.fit(*get_fit_data(db_dir, dataset))
+    predictor.fit(*fit_data)
     scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
     predictions = predictor.predict(scores)
     np.testing.assert_array_equal(predictions, np.array([1, 0, 1]))
 
 
-def test_predict_handles_single_class(dataset):
-    db_dir, dump_dir, logs_dir = setup_environment()
-
+def test_predict_handles_single_class(fit_data):
     predictor = ThresholdPredictor(0.5)
-    predictor.fit(*get_fit_data(db_dir, dataset))
+    predictor.fit(*fit_data)
     scores = np.array([[0.5], [0.5], [0.5]])
     predictions = predictor.predict(scores)
     np.testing.assert_array_equal(predictions, np.array([0, 0, 0]))
