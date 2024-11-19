a9075c535fdba9b6e3a0477f9936ad7693cba635
voorhs <ilya_alekseev_2016@list.ru>
2024-11-19 12:33:31 +0300

    fix jinoos predictor tests

--- tests/modules/prediction/test_jinoos.py
+++ tests/modules/prediction/test_jinoos.py
@@ -1,22 +1,24 @@
 import numpy as np
-import pytest
 
 from autointent.modules import JinoosPredictor
 
 
-@pytest.mark.xfail
-def test_predict_returns_correct_indices(context):
+def detect_oos(scores, labels, thresh):
+    """
+    `labels`: labels without oos detection
+    """
+    mask = np.max(scores, axis=1) < thresh
+    labels[mask] = -1
+    return labels
+
+def test_predict_returns_correct_indices(multiclass_fit_data):
     predictor = JinoosPredictor()
-    predictor.fit(context("multiclass"))
+    predictor.fit(*multiclass_fit_data)
     scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
-    predictions = predictor.predict(scores)
-    np.testing.assert_array_equal(predictions, np.array([1, 0, 1]))
 
-
-@pytest.mark.xfail
-def test_predict_handles_single_class(context):
-    predictor = JinoosPredictor()
-    predictor.fit(context("multiclass"))
-    scores = np.array([[0.5], [0.5], [0.5]])
+    # inference
     predictions = predictor.predict(scores)
-    np.testing.assert_array_equal(predictions, np.array([0, 0, 0]))
+    desired = detect_oos(scores, np.array([1,0,1]), predictor.thresh)
+
+    np.testing.assert_array_equal(predictions, desired)
+
