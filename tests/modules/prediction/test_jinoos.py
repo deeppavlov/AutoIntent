4d8c0071d6a3f8237bfe6a08b6d9317065a6c2fa
voorhs <ilya_alekseev_2016@list.ru>
2024-11-19 14:40:12 +0300

    add tests

--- tests/modules/prediction/test_jinoos.py
+++ tests/modules/prediction/test_jinoos.py
@@ -1,24 +1,44 @@
+from typing import Any
+
 import numpy as np
+import numpy.typing as npt
+import pytest
 
 from autointent.modules import JinoosPredictor
+from autointent.modules.prediction.utils import InvalidNumClassesError, WrongClassificationError
 
 
-def detect_oos(scores, labels, thresh):
+def detect_oos(scores: npt.NDArray[Any], labels: npt.NDArray[Any], thresh: float):
     """
     `labels`: labels without oos detection
     """
     mask = np.max(scores, axis=1) < thresh
     labels[mask] = -1
+
     return labels
+
 
 def test_predict_returns_correct_indices(multiclass_fit_data):
     predictor = JinoosPredictor()
     predictor.fit(*multiclass_fit_data)
-    scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
+    scores = np.array([[0.1, 0.9, 0], [0.8, 0, 0.2], [0, 0.3, 0.7]])
 
     # inference
     predictions = predictor.predict(scores)
-    desired = detect_oos(scores, np.array([1,0,1]), predictor.thresh)
+    desired = detect_oos(scores, np.array([1, 0, 2]), predictor.thresh)
 
     np.testing.assert_array_equal(predictions, desired)
 
+
+def test_fails_on_wrong_n_classes(multiclass_fit_data):
+    predictor = JinoosPredictor()
+    predictor.fit(*multiclass_fit_data)
+    scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
+    with pytest.raises(InvalidNumClassesError):
+        predictor.predict(scores)
+
+
+def test_fails_on_wrong_clf_problem(multilabel_fit_data):
+    predictor = JinoosPredictor()
+    with pytest.raises(WrongClassificationError):
+        predictor.fit(*multilabel_fit_data)
--- tests/modules/prediction/test_threshold.py
+++ tests/modules/prediction/test_threshold.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pytest
 
 from autointent.modules import ThresholdPredictor
+from autointent.modules.prediction.utils import InvalidNumClassesError
 
 
-def test_predict_returns_correct_indices(multiclass_fit_data):
+def test_multiclass(multiclass_fit_data):
     predictor = ThresholdPredictor(0.5)
     predictor.fit(*multiclass_fit_data)
     scores = np.array([[0.1, 0.9, 0], [0.8, 0, 0.2], [0, 0.3, 0.7]])
@@ -11,9 +13,39 @@ def test_predict_returns_correct_indices(multiclass_fit_data):
     np.testing.assert_array_equal(predictions, np.array([1, 0, 2]))
 
 
-def test_predict_returns_list(multiclass_fit_data):
+def test_multilabel(multilabel_fit_data):
+    predictor = ThresholdPredictor(thresh=0.5)
+    predictor.fit(*multilabel_fit_data)
+    scores = np.array([[0.2, 0.9, 0], [0.8, 0, 0.6], [0, 0.4, 0.7]])
+    predictions = predictor.predict(scores)
+    np.testing.assert_array_equal(predictions, np.array([[0, 1, 0], [1, 0, 1], [0, 0, 1]]))
+
+
+def test_multiclass_list(multiclass_fit_data):
     predictor = ThresholdPredictor(np.array([0.5, 0.5, 0.8]))
     predictor.fit(*multiclass_fit_data)
     scores = np.array([[0.1, 0.9, 0], [0.8, 0, 0.2], [0, 0.3, 0.7]])
     predictions = predictor.predict(scores)
     np.testing.assert_array_equal(predictions, np.array([1, 0, -1]))
+
+
+def test_multilabel_list(multilabel_fit_data):
+    predictor = ThresholdPredictor(np.array([0.5, 0.5, 0.8]))
+    predictor.fit(*multilabel_fit_data)
+    scores = np.array([[0.1, 0.9, 0], [0.8, 0, 0.2], [0, 0.3, 0.7]])
+    predictions = predictor.predict(scores)
+    np.testing.assert_array_equal(predictions, np.array([[0, 1, 0], [1, 0, 0], [0, 0, 0]]))
+
+
+def test_fails_on_wrong_n_classes_predict(multiclass_fit_data):
+    predictor = ThresholdPredictor(thresh=0.5)
+    predictor.fit(*multiclass_fit_data)
+    scores = np.array([[0.1, 0.9], [0.8, 0.2], [0.3, 0.7]])
+    with pytest.raises(InvalidNumClassesError):
+        predictor.predict(scores)
+
+
+def test_fails_on_wrong_n_classes_fit(multiclass_fit_data):
+    predictor = ThresholdPredictor(thresh=[0.5])
+    with pytest.raises(InvalidNumClassesError):
+        predictor.fit(*multiclass_fit_data)
