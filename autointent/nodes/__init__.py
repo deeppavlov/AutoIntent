4372d0f9a832f124e8bff5df9af570acd893e08f
voorhs <ilya_alekseev_2016@list.ru>
2024-12-09 02:57:22 +0300

    fix curcular import error

--- autointent/nodes/__init__.py
+++ autointent/nodes/__init__.py
@@ -1,11 +1,10 @@
 """Some core components used in AutoIntent behind the scenes."""
 
 from ._inference_node import InferenceNode
-from ._nodes_info import NODES_INFO, NodeInfo, PredictionNodeInfo, RegExpNodeInfo, RetrievalNodeInfo, ScoringNodeInfo
+from ._nodes_info import NodeInfo, PredictionNodeInfo, RegExpNodeInfo, RetrievalNodeInfo, ScoringNodeInfo
 from ._optimization import NodeOptimizer
 
 __all__ = [
-    "NODES_INFO",
     "InferenceNode",
     "NodeInfo",
     "NodeOptimizer",
