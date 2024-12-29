9c02bb4199ece48e4ec592f5ba4ebbb40f182be3
Dmitry Zhelobanov <dmitry.v.zhelobanov@yandex.ru>
2024-12-30 00:10:38 +0400

    Still fixing doc build

--- autointent/modules/scoring/_dnnc/dnnc.py
+++ autointent/modules/scoring/_dnnc/dnnc.py
@@ -79,8 +79,8 @@ class DNNCScorer(ScoringModule):
 
     .. testoutput::
 
-        [[0.00013581  0.        ]
-         [0.00030066  0.        ]]
+        [[0.00013582 0.        ]
+         [0.00030066 0.        ]]
 
     .. testcleanup::
 
