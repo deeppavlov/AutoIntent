2f2ef32ca0c6e6b490c5fa7f2c891c3cccc84ee7
voorhs <ilya_alekseev_2016@list.ru>
2025-01-16 19:11:49 +0300

    fix custom log level issue

--- autointent/_logging/setup.py
+++ autointent/_logging/setup.py
@@ -1,15 +1,18 @@
 import importlib.resources as ires
-import json
 import logging.config
 import logging.handlers
+
+import yaml
 
 from autointent.custom_types import LogLevel
 
 
-def setup_logging(level: LogLevel) -> None:
-    config_file = ires.files("autointent._logging").joinpath("config.json")
+def setup_logging(level: LogLevel | str) -> None:
+    config_file = ires.files("autointent._logging").joinpath("config.yaml")
     with config_file.open() as f_in:
-        config = json.load(f_in)
+        config = yaml.safe_load(f_in)
+
+    level = LogLevel(level)
+    config["loggers"]["root"]["level"] = level.value
 
     logging.config.dictConfig(config)
-    logging.basicConfig(level=level.value)
