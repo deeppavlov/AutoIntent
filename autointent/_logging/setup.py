7ed133a87a3d71d8abf43c19c3f36b01fe84644a
voorhs <ilya_alekseev_2016@list.ru>
2025-01-16 19:32:30 +0300

    add logging to file by demand

--- autointent/_logging/setup.py
+++ autointent/_logging/setup.py
@@ -1,18 +1,29 @@
 import importlib.resources as ires
 import logging.config
 import logging.handlers
+from pathlib import Path
 
 import yaml
 
 from autointent.custom_types import LogLevel
 
 
-def setup_logging(level: LogLevel | str) -> None:
+def setup_logging(level: LogLevel | str, log_to_filepath: Path | str | None = None) -> None:
     config_file = ires.files("autointent._logging").joinpath("config.yaml")
     with config_file.open() as f_in:
         config = yaml.safe_load(f_in)
 
     level = LogLevel(level)
-    config["loggers"]["root"]["level"] = level.value
+    config["handlers"]["stdout"]["level"] = level.value
+
+    if log_to_filepath is not None:
+        config["loggers"]["root"]["handlers"].append("file")
+        config["handlers"]["file"] = {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": "json",
+            "filename": str(log_to_filepath),
+        }
+
 
     logging.config.dictConfig(config)
