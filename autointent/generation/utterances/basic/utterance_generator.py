7691a9c61e62421b22c35cd262c945eb04e4bfa4
voorhs <ilya_alekseev_2016@list.ru>
2025-01-18 13:33:40 +0300

    refactor basic utterance generator

--- autointent/generation/utterances/basic/utterance_generator.py
+++ autointent/generation/utterances/basic/utterance_generator.py
@@ -1,3 +1,5 @@
+"""Basic generation of new utterances from existing ones."""
+
 import importlib.resources as ires
 import json
 from typing import Any, Literal
@@ -12,6 +14,14 @@ StyleType = Literal["none", "formal", "informal", "playful"]
 
 
 class UtteranceGenerator:
+    """
+    Basic generation of new utterances from existing ones.
+
+    This augmentation method simply prompts LLM to look at existing examples
+    and generate similar. Additionaly it can consider some aspects of style,
+    punctuation and length of the desired generations.
+    """
+
     def __init__(
         self,
         generator: Generator,
@@ -19,10 +29,11 @@ class UtteranceGenerator:
         length: LengthType,
         style: StyleType,
         same_punctuation: bool,
-    ):
+    ) -> None:
+        """Initialize."""
         self.generator = generator
-        prompt_template_yaml = load_prompt()
-        self.prompt_template_yaml = add_extra_instructions(
+        prompt_template_yaml = _load_prompt()
+        self.prompt_template_yaml = _add_extra_instructions(
             prompt_template_yaml,
             custom_instruction,
             length,
@@ -30,44 +41,37 @@ class UtteranceGenerator:
             same_punctuation,
         )
 
-    def _generate(self, intent_name: str, example_utterances: list[str], n_examples: int) -> list[str]:
+    def __call__(self, intent_name: str, example_utterances: list[str], n_generations: int) -> list[str]:
+        """Generate new utterances."""
         messages_yaml = safe_format(
             self.prompt_template_yaml,
             intent_name=intent_name,
-            example_utterances=format_utterances(example_utterances),
-            n_examples=n_examples,
+            example_utterances=_format_utterances(example_utterances),
+            n_examples=n_generations,
         )
         messages = yaml.safe_load(messages_yaml)
         response_text = self.generator.get_chat_completion(messages)
-        return extract_utterances(response_text)
-
-    def __call__(self, intent_record: dict[str, Any], n_examples: int, inplace: bool = True) -> list[str]:
-        intent_name = intent_record.get("intent_name", "")
-        example_utterances = intent_record.get("sample_utterances", [])
-        res_utterances = self._generate(intent_name, example_utterances, n_examples)
-        if inplace:
-            intent_record["sample_utterances"] = intent_record.get("sample_utterances", []) + res_utterances
-        return res_utterances
+        return _extract_utterances(response_text)
 
 
-def load_prompt():
+def _load_prompt() -> str:
     with ires.files("autointent.generation.basic").joinpath("chat_template.yaml").open() as file:
         return file.read()
 
 
-def load_extra_instructions():
+def _load_extra_instructions() -> dict[str, Any]:
     with ires.files("autointent.generation.basic").joinpath("extra_instructions.json").open() as file:
         return json.load(file)
 
 
-def add_extra_instructions(
+def _add_extra_instructions(
     prompt_template_yaml: str,
     custom_instruction: list[str],
     length: LengthType,
     style: StyleType,
     same_punctuation: bool,
 ) -> str:
-    instructions = load_extra_instructions()
+    instructions = _load_extra_instructions()
 
     extra_instructions = []
     if length != "none":
@@ -83,37 +87,26 @@ def add_extra_instructions(
     return safe_format(prompt_template_yaml, extra_instructions=parsed_extra_instructions)
 
 
-def format_utterances(utterances: list[str]) -> str:
+def _format_utterances(utterances: list[str]) -> str:
     """
-    Return
-    ---
-    str of the following format:
+    Convert given utterances into string that is ready to insert into prompt.
 
-    ```
+    Given list of utterances, the output string is returned in the following format:
+    .. code-block::
         1. I want to order a large pepperoni pizza.
         2. Can I get a medium cheese pizza with extra olives?
         3. Please deliver a small veggie pizza to my address.
-    ```
 
-    Note
-    ---
-    tab is inserted before each line because of how yaml processes multi-line fields
+    Note that tab is inserted before each line because of how yaml processes multi-line fields.
     """
     return "\n    ".join(f"{i}. {ut}" for i, ut in enumerate(utterances))
 
 
-def extract_utterances(response_text: str) -> list[str]:
+def _extract_utterances(response_text: str) -> list[str]:
     """
-    Input
-    ---
-    str of the following format:
+    Parse LLM output.
 
-    ```
-    1. I want to order a large pepperoni pizza.
-    2. Can I get a medium cheese pizza with extra olives?
-    3. Please deliver a small veggie pizza to my address.
-    ```
-
+    Inverse function to :py:func:`_format_utterances`.
     """
     raw_utterances = response_text.split("\n")
     # remove enumeration
