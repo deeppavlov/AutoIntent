8c5522e603f32facc98cf5a3bd7d752941525956
voorhs <ilya_alekseev_2016@list.ru>
2025-01-21 13:09:57 +0300

    stage progress

--- autointent/context/data_handler/_stratification.py
+++ autointent/context/data_handler/_stratification.py
@@ -8,6 +8,7 @@ from collections.abc import Sequence
 
 import numpy as np
 from datasets import Dataset as HFDataset
+from datasets import concatenate_datasets
 from numpy import typing as npt
 from sklearn.model_selection import train_test_split
 from skmultilearn.model_selection import IterativeStratification
@@ -44,33 +45,109 @@ class StratifiedSplitter:
         self.random_seed = random_seed
         self.shuffle = shuffle
 
-    def __call__(self, dataset: HFDataset, multilabel: bool) -> tuple[HFDataset, HFDataset]:
+    def __call__(
+        self, dataset: HFDataset, multilabel: bool, allow_oos_in_train: bool = False
+    ) -> tuple[HFDataset, HFDataset]:
         """
         Split the dataset into training and testing subsets.
 
         :param dataset: The input dataset to be split.
         :param multilabel: Whether the dataset is multi-label.
+        :param allow_oos_in_train: Set to True if you want to see out-of-scope utterances in train split.
         :return: A tuple containing the training and testing datasets.
         """
-        splits = self._split_multilabel(dataset) if multilabel else self._split(dataset)
+        if not self._has_oos_samples(dataset):
+            return self._split_without_oos(dataset, multilabel, self.test_size)
+        splitter = self._split_allow_oos_in_train if allow_oos_in_train else self._split_disallow_oos_in_train
+        return splitter(dataset, multilabel)
+
+    def _has_oos_samples(self, dataset: HFDataset) -> bool:
+        oos_samples = dataset.filter(lambda sample: sample[self.label_feature] is None)
+        return len(oos_samples) > 0
+
+    def _split_without_oos(self, dataset: HFDataset, multilabel: bool, test_size: bool) -> tuple[HFDataset, HFDataset]:
+        splitter = self._split_multilabel if multilabel else self._split_multiclass
+        splits = splitter(dataset, test_size)
         return dataset.select(splits[0]), dataset.select(splits[1])
 
-    def _split(self, dataset: HFDataset) -> Sequence[npt.NDArray[np.int_]]:
+    def _split_multiclass(self, dataset: HFDataset, test_size: float) -> Sequence[npt.NDArray[np.int_]]:
         return train_test_split(  # type: ignore[no-any-return]
             np.arange(len(dataset)),
-            test_size=self.test_size,
+            test_size=test_size,
             random_state=self.random_seed,
             shuffle=self.shuffle,
             stratify=dataset[self.label_feature],
         )
 
-    def _split_multilabel(self, dataset: HFDataset) -> Sequence[npt.NDArray[np.int_]]:
+    def _split_multilabel(self, dataset: HFDataset, test_size: float) -> Sequence[npt.NDArray[np.int_]]:
         splitter = IterativeStratification(
             n_splits=2,
             order=2,
-            sample_distribution_per_fold=[self.test_size, 1.0 - self.test_size],
+            sample_distribution_per_fold=[test_size, 1.0 - test_size],
         )
         return next(splitter.split(np.arange(len(dataset)), np.array(dataset[self.label_feature])))
+
+    def _split_allow_oos_in_train(self, dataset: HFDataset, multilabel: bool) -> tuple[HFDataset, HFDataset]:
+        """
+        Equally distribute OOS samples between two splits.
+
+        Internally, this method creates a dataset copy with some integer assigned as OOS class id.
+        With OOS samples treated as a separate class we obtain proportional distribution of them between two splits.
+        """
+        oos_class_id = len(dataset.unique(self.label_feature)) - 1
+        dataset = dataset.map(self._map_label, fn_kwargs={"old": None, "new": oos_class_id})
+        train, test = self._split_without_oos(dataset, multilabel, self.test_size)
+        train = train.map(self._map_label, fn_kwargs={"old": oos_class_id, "new": None})
+        test = test.map(self._map_label, fn_kwargs={"old": oos_class_id, "new": None})
+        return train, test
+
+    def _map_label(
+        self, sample: dict[str, str | int | None], old: int | None, new: int | None
+    ) -> dict[str, str | int | None]:
+        if sample[self.label_feature] == old:
+            sample[self.label_feature] = new
+        return sample
+
+    def _split_disallow_oos_in_train(self, dataset: HFDataset, multilabel: bool) -> tuple[HFDataset, HFDataset]:
+        """
+        Move all OOS samples to test split.
+
+        This method preserves the defined test_size proportion so you won't get unexpectedly
+        large test set even you have lots of OOS samples.
+        """
+        in_domain_dataset, out_of_domain_dataset = self._separate_oos(dataset)
+        adjusted_test_size = self._get_adjusted_test_size(len(dataset), len(out_of_domain_dataset))
+        train, test = self._split_without_oos(in_domain_dataset, multilabel, adjusted_test_size)
+        test = concatenate_datasets([test, out_of_domain_dataset])
+        return train, test
+
+    def _separate_oos(self, dataset: HFDataset) -> tuple[HFDataset, HFDataset]:
+        in_domain_ids = []
+        out_of_domain_ids = []
+        for i, sample in enumerate(dataset):
+            if sample[self.label_feature] is None:
+                out_of_domain_ids.append(i)
+            else:
+                in_domain_ids.append(i)
+        return dataset.select(in_domain_ids), dataset.select(out_of_domain_ids)
+
+    def _get_adjusted_test_size(self, n: int, k: int) -> float:
+        """
+        Calculate effective test_size in order to preserve original proportion.
+
+        :param n: size of original dataset (both with in-domain and out-of-domain samples)
+        :param k: number of out-of-domain samples within the dataset
+        """
+        if k == 0:
+            return self.test_size
+        res = (self.test_size * n - k) / (n - k)
+        if res <= 0:
+            msg = (
+                "Error while splitting dataset. Dataset contains too many OOS samples. "
+                "Either increase test_size or decrease number of OOS samples."
+            )
+            raise ValueError(msg)
+        return res
 
 
 def split_dataset(
