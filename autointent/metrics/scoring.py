20acd67f3baaae979ba4e94430145bae113a5b9b
voorhs <ilya_alekseev_2016@list.ru>
2025-01-23 00:51:07 +0300

    fix some metric

--- autointent/metrics/scoring.py
+++ autointent/metrics/scoring.py
@@ -114,14 +114,12 @@ def _calculate_decision_metric(func: DecisionMetricFn, labels: LABELS_VALUE_TYPE
     :param scores: for each utterance, this list contains scores for each of `n_classes` classes
     :return: Score of the scoring metric
     """
-    labels_, scores_ = transform(labels, scores)
-
-    if labels_.ndim == 1:
-        pred_labels = np.argmax(scores, axis=1)
-        res = func(labels_, pred_labels)
+    if isinstance(labels[0], int):
+        pred_labels = np.argmax(scores, axis=1).tolist()
+        res = func(labels, pred_labels)
     else:
-        pred_labels = (scores_ > 0.5).astype(int)  # noqa: PLR2004
-        res = func(labels_, pred_labels)
+        pred_labels = (np.array(scores) > 0.5).astype(int).tolist()  # noqa: PLR2004
+        res = func(labels, pred_labels)
 
     return res
 
