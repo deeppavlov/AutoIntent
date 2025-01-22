0e3eeeec8b151fbc80bc1862b682bc00fca0c6fa
voorhs <ilya_alekseev_2016@list.ru>
2025-01-22 19:58:21 +0300

    bug fix in test

--- tests/modules/decision/conftest.py
+++ tests/modules/decision/conftest.py
@@ -16,7 +16,7 @@ def multiclass_fit_data(dataset):
     }
     scorer = KNNScorer(**knn_params)
 
-    scorer.fit(data_handler.train_utterances(1), data_handler.train_labels(1))
+    scorer.fit(data_handler.train_utterances(0), data_handler.train_labels(0))
     scores = scorer.predict(data_handler.validation_utterances(1))
     labels = data_handler.validation_labels(1)
     return scores, labels
@@ -33,7 +33,7 @@ def multilabel_fit_data(dataset):
     }
     scorer = KNNScorer(**knn_params)
 
-    scorer.fit(data_handler.train_utterances(1), data_handler.train_labels(1))
+    scorer.fit(data_handler.train_utterances(0), data_handler.train_labels(0))
     scores = scorer.predict(data_handler.validation_utterances(1))
     labels = data_handler.validation_labels(1)
     return scores, labels
