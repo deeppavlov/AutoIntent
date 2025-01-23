662f8126e277c08aab4d8158510af3b4049a7ff7
voorhs <ilya_alekseev_2016@list.ru>
2025-01-23 13:21:02 +0300

    remove unnecessary comments

--- autointent/modules/abc/_base.py
+++ autointent/modules/abc/_base.py
@@ -120,10 +120,7 @@ class Module(ABC):
         """
         metrics = {}
         for metric_name, metric_fn in metrics_dict.items():
-            # try:
             metrics[metric_name] = metric_fn(*params)
-            # except Exception as e:  # nqa: PERF203, BLE001
-            #     metrics[metric_name] = str(e)
         return metrics
 
     def _validate_multilabel(self, data_is_multilabel: bool) -> None:
