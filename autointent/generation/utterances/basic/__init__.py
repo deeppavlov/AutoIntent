06d1d58bb0c89423e1c54aa31f96599706b37a1a
voorhs <ilya_alekseev_2016@list.ru>
2025-01-31 12:17:57 +0300

    configure explicit import from `generation.utterances` submodule

---
+++ autointent/generation/utterances/basic/__init__.py
@@ -0,0 +1,4 @@
+from .chat_template import SynthesizerChatTemplate
+from .utterance_generator import UtteranceGenerator
+
+__all__ = ["SynthesizerChatTemplate", "UtteranceGenerator"]
---
+++ autointent/generation/utterances/evolution/__init__.py
@@ -0,0 +1,4 @@
+from .chat_templates import AbstractEvolution, ConcreteEvolution, EvolutionChatTemplate, ReasoningEvolution
+from .evolver import UtteranceEvolver
+
+__all__ = ["AbstractEvolution", "ConcreteEvolution", "EvolutionChatTemplate", "ReasoningEvolution", "UtteranceEvolver"]
