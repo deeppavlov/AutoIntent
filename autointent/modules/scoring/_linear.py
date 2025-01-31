Keep fixing documentation build fails due to precision in the output

autointent/modules/scoring/_linear.py
@@ -25,6 +25,7 @@ class LinearScorer(ScoringModule):
     --------
     .. testcode::
 
+        import numpy as np
         from autointent.modules import LinearScorer
         scorer = LinearScorer(
             embedder_name="sergeyzh/rubert-tiny-turbo", cv=2
@@ -34,12 +35,13 @@ class LinearScorer(ScoringModule):
         scorer.fit(utterances, labels)
         test_utterances = ["hi", "bye"]
         probabilities = scorer.predict(test_utterances)
-        print(probabilities)
+        with np.printoptions(precision=4):
+            print(probabilities)
 
     .. testoutput::
 
-        [[0.50000032 0.49999968]
-         [0.50000032 0.49999968]]
+        [[0.5 0.5]]
+         [0.5 0.5]]
 
     """
 

autointent/modules/scoring/_mlknn/mlknn.py
@@ -24,6 +24,7 @@ class MLKnnScorer(ScoringModule):
 
     .. testcode::
 
+        import numpy as np
         from autointent.modules.scoring import MLKnnScorer
         utterances = ["what is your name?", "how are you?"]
         labels = [[1,0], [0,1]]
@@ -34,7 +35,8 @@ class MLKnnScorer(ScoringModule):
         scorer.fit(utterances, labels)
         test_utterances = ["Hi!", "What's up?"]
         probabilities = scorer.predict(test_utterances)
-        print(probabilities)  # Outputs predicted probabilities for each label
+        with np.printoptions(precision=4):
+            print(probabilities)  # Outputs predicted probabilities for each label
 
     .. testoutput::
 