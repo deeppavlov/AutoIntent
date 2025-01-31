Fixing documentation build fails due to precision in the output

autointent/modules/scoring/_knn/knn.py
@@ -28,6 +28,7 @@ class KNNScorer(ScoringModule):
 
     .. testcode::
 
+        import numpy as np
         from autointent.modules.scoring import KNNScorer
         utterances = ["hello", "how are you?"]
         labels = [0, 1]
@@ -38,12 +39,13 @@ class KNNScorer(ScoringModule):
         scorer.fit(utterances, labels)
         test_utterances = ["hi", "what's up?"]
         probabilities = scorer.predict(test_utterances)
-        print(probabilities)  # Outputs predicted class probabilities for the utterances
+        with np.printoptions(precision=4):
+            print(probabilities)  # Outputs predicted class probabilities for the utterances
 
     .. testoutput::
 
-        [[0.67297815 0.32702185]
-         [0.44031678 0.55968322]]
+        [[0.673  0.327 ]
+         [0.4403 0.5597]]
 
     """
 