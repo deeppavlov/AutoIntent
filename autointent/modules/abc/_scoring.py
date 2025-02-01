8019153e53e9696a1d67d77065a3201f742f9246
voorhs <ilya_alekseev_2016@list.ru>
2025-02-01 12:03:27 +0300

    define interface

--- autointent/modules/abc/_scoring.py
+++ autointent/modules/abc/_scoring.py
@@ -1,8 +1,9 @@
 """Base class for scoring modules."""
 
 from abc import ABC, abstractmethod
-from typing import Any, Literal
+from typing import Any
 
+import numpy as np
 import numpy.typing as npt
 
 from autointent import Context
@@ -21,7 +22,7 @@ class ScoringModule(Module, ABC):
 
     supports_oos = False
 
-    def score(self, context: Context, split: Literal["validation", "test"], metrics: list[str]) -> dict[str, float]:
+    def score(self, context: Context, test: bool, metrics: list[str]) -> dict[str, float]:
         """
         Evaluate the scorer on a test set and compute the specified metric.
 
@@ -29,25 +30,23 @@ class ScoringModule(Module, ABC):
         :param split: Target split
         :return: Computed metrics value for the test set or error code of metrics
         """
-        if split == "validation":
-            utterances = context.data_handler.validation_utterances(0)
-            labels = context.data_handler.validation_labels(0)
-        elif split == "test":
-            utterances = context.data_handler.test_utterances()
-            labels = context.data_handler.test_labels()
-        else:
-            message = f"Invalid split '{split}' provided. Expected one of 'validation', or 'test'."
-            raise ValueError(message)
-
-        scores = self.predict(utterances)
-
-        self._train_scores = self.predict(context.data_handler.train_utterances(1))
-        self._validation_scores = self.predict(context.data_handler.validation_utterances(1))
-        self._test_scores = self.predict(context.data_handler.test_utterances())
-
         metrics_dict = SCORING_METRICS_MULTILABEL if context.is_multilabel() else SCORING_METRICS_MULTICLASS
         chosen_metrics = {name: fn for name, fn in metrics_dict.items() if name in metrics}
-        return self.score_metrics((labels, scores), chosen_metrics)
+
+        if test:
+            utterances = context.data_handler.test_utterances()
+            labels = context.data_handler.test_labels()
+            scores = self.predict(utterances)
+            return self.score_metrics((labels, scores), chosen_metrics)
+
+        metrics_values = {name: [] for name in chosen_metrics}
+        for train_utterances, train_labels, val_utterances, val_labels in context.validation_iterator(0):
+            self.fit(train_utterances, train_labels)
+            val_scores = self.predict(val_utterances)
+            for name, fn in chosen_metrics.items():
+                metrics_values[name].append(fn(val_labels, val_scores))
+
+        return {name: np.mean(values_list) for name, values_list in metrics_values.items()}
 
     def get_assets(self) -> ScorerArtifact:
         """
