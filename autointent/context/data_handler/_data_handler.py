30847185bad87848dc422844ea4f953cb7a7aa23
voorhs <ilya_alekseev_2016@list.ru>
2025-02-05 10:06:52 +0300

    fix codestyle

--- autointent/context/data_handler/_data_handler.py
+++ autointent/context/data_handler/_data_handler.py
@@ -249,9 +249,9 @@ class DataHandler:  # TODO rename to Validator
         )
 
     def _split_cv(self, random_seed: int) -> None:
-        self.dataset[Split.TRAIN] = concatenate_datasets([
-            self.dataset[split_name] for split_name in self.dataset if split_name not in [Split.TRAIN, Split.TEST]
-        ])
+        self.dataset[Split.TRAIN] = concatenate_datasets(
+            [self.dataset[split_name] for split_name in self.dataset if split_name not in [Split.TRAIN, Split.TEST]]
+        )
 
         if Split.TEST not in self.dataset:
             self.dataset[Split.TRAIN], self.dataset[Split.TEST] = split_dataset(
