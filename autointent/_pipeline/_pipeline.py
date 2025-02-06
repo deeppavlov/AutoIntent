d22b2db8132b529ea56944fc4ef9c905cb089d12
voorhs <ilya_alekseev_2016@list.ru>
2025-02-06 15:34:14 +0300

    bug fix

--- autointent/_pipeline/_pipeline.py
+++ autointent/_pipeline/_pipeline.py
@@ -229,10 +229,10 @@ class Pipeline:
 
         context.data_handler.prepare_for_refit()
 
-        scoring_module.fit(context.data_handler.train_utterances(0), context.data_handler.train_labels(0))  # type: ignore[arg-type]
+        scoring_module.fit(*scoring_module.get_train_data(context))  # type: ignore[arg-type]
         scores = scoring_module.predict(context.data_handler.train_utterances(1))
 
-        decision_module.fit(scores, context.data_handler.train_labels(1))
+        decision_module.fit(scores, context.data_handler.train_labels(1), context.data_handler.tags)
 
     def predict_with_metadata(self, utterances: list[str]) -> InferencePipelineOutput:
         """
