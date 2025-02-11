5ed5ac32d3dd773634a956ccc6722ad5f47c68e1
voorhs <ilya_alekseev_2016@list.ru>
2025-02-11 21:56:31 +0300

    add test on bayes

--- tests/pipeline/test_optimization.py
+++ tests/pipeline/test_optimization.py
@@ -1,5 +1,4 @@
 import os
-from typing import Literal
 
 import pytest
 
@@ -11,7 +10,18 @@ from autointent.configs import (
 )
 from tests.conftest import get_search_space, setup_environment
 
-TaskType = Literal["multiclass", "multilabel", "description"]
+
+def test_bayes(dataset):
+    project_dir = setup_environment()
+    search_space = get_search_space("bayes")
+
+    pipeline_optimizer = Pipeline.from_search_space(search_space)
+
+    pipeline_optimizer.set_config(LoggingConfig(project_dir=project_dir, dump_modules=True, clear_ram=True))
+    pipeline_optimizer.set_config(VectorIndexConfig())
+    pipeline_optimizer.set_config(EmbedderConfig(batch_size=16, max_length=32, device="cuda"))
+
+    pipeline_optimizer.fit(dataset, scheme="cv", refit_after=True)
 
 
 @pytest.mark.parametrize(
