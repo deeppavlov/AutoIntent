29357930754622260ce7514df26f89fb2797940a
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-02-22 17:27:08 +0300

    fix imports

--- autointent/generation/utterances/evolution/evolver.py
+++ autointent/generation/utterances/evolution/evolver.py
@@ -13,7 +13,7 @@ from datasets import concatenate_datasets
 
 from autointent import Dataset
 from autointent.custom_types import Split
-from autointent.generation.utterances import EvolutionChatTemplate
+from autointent.generation.utterances.evolution.chat_templates import EvolutionChatTemplate
 from autointent.generation.utterances.generator import Generator
 from autointent.schemas import Intent
 
--- autointent/generation/utterances/evolution/incremental_evolver.py
+++ autointent/generation/utterances/evolution/incremental_evolver.py
@@ -14,7 +14,7 @@ from datasets import concatenate_datasets
 
 from autointent import Dataset, Pipeline
 from autointent.custom_types import Split
-from autointent.generation.utterances import EvolutionChatTemplate
+from autointent.generation.utterances.evolution.chat_templates import EvolutionChatTemplate
 from autointent.generation.utterances.evolution.evolver import UtteranceEvolver
 from autointent.generation.utterances.generator import Generator
 
