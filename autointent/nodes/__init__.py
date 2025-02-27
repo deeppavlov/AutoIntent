1680d269466850445a0ad1322de7f6359ec861be
voorhs <ilya_alekseev_2016@list.ru>
2025-02-27 16:19:44 +0300

    remove previous validation

--- autointent/nodes/__init__.py
+++ autointent/nodes/__init__.py
@@ -2,10 +2,8 @@
 
 from ._inference_node import InferenceNode
 from ._optimization import NodeOptimizer
-from .schemes import OptimizationSearchSpaceConfig
 
 __all__ = [
     "InferenceNode",
     "NodeOptimizer",
-    "OptimizationSearchSpaceConfig",
 ]
