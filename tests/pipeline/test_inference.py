2d0dd569a5b2b7c90096dc137a5b36a742e31084
voorhs <ilya_alekseev_2016@list.ru>
2025-02-28 13:06:57 +0300

    update tests

--- tests/pipeline/test_inference.py
+++ tests/pipeline/test_inference.py
@@ -1,7 +1,8 @@
 import pytest
 
 from autointent import Pipeline
-from autointent.configs import LoggingConfig
+from autointent.configs import EmbedderConfig, LoggingConfig
+from autointent.custom_types import NodeType
 from tests.conftest import get_search_space, setup_environment
 
 
@@ -9,21 +10,22 @@ from tests.conftest import get_search_space, setup_environment
     "task_type",
     ["multiclass", "multilabel", "description"],
 )
-def test_inference_config(dataset, task_type):
+def test_inference_from_config(dataset, task_type):
     project_dir = setup_environment()
     search_space = get_search_space(task_type)
 
     pipeline_optimizer = Pipeline.from_search_space(search_space)
 
-    pipeline_optimizer.set_config(LoggingConfig(project_dir=project_dir, dump_modules=True, clear_ram=True))
+    logging_config = LoggingConfig(project_dir=project_dir, dump_modules=True, clear_ram=True)
+    pipeline_optimizer.set_config(logging_config)
 
     if task_type == "multilabel":
         dataset = dataset.to_multilabel()
 
     context = pipeline_optimizer.fit(dataset)
-    inference_config = context.optimization_info.get_inference_nodes_config()
+    context.dump()
 
-    inference_pipeline = Pipeline.from_config(inference_config)
+    inference_pipeline = Pipeline.load(logging_config.dirpath)
     utterances = ["123", "hello world"]
     prediction = inference_pipeline.predict(utterances)
     assert len(prediction) == 2
@@ -31,14 +33,12 @@ def test_inference_config(dataset, task_type):
     rich_outputs = inference_pipeline.predict_with_metadata(utterances)
     assert len(rich_outputs.predictions) == len(utterances)
 
-    context.dump()
-
 
 @pytest.mark.parametrize(
     "task_type",
     ["multiclass", "multilabel", "description"],
 )
-def test_inference_context(dataset, task_type):
+def test_inference_on_the_fly(dataset, task_type):
     project_dir = setup_environment()
     search_space = get_search_space(task_type)
 
@@ -59,3 +59,29 @@ def test_inference_context(dataset, task_type):
     assert len(rich_outputs.predictions) == len(utterances)
 
     context.dump()
+
+
+def test_load_with_overrided_params(dataset):
+    project_dir = setup_environment()
+    search_space = get_search_space("light")
+
+    pipeline_optimizer = Pipeline.from_search_space(search_space)
+
+    logging_config = LoggingConfig(project_dir=project_dir, dump_modules=True, clear_ram=True)
+    pipeline_optimizer.set_config(logging_config)
+
+    context = pipeline_optimizer.fit(dataset)
+    context.dump()
+
+    inference_pipeline = Pipeline.load(logging_config.dirpath, embedder_config=EmbedderConfig(max_length=8))
+    utterances = ["123", "hello world"]
+    prediction = inference_pipeline.predict(utterances)
+    assert len(prediction) == 2
+
+    rich_outputs = inference_pipeline.predict_with_metadata(utterances)
+    assert len(rich_outputs.predictions) == len(utterances)
+
+    assert inference_pipeline.nodes[NodeType.scoring].module._embedder.max_length == 8
+
+
+# TODO Pipeline.dump()
