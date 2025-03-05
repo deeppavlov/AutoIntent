2c6dff39cef0fef098f2a1f2ce69dc997c31c349
voorhs <ilya_alekseev_2016@list.ru>
2025-03-05 13:10:36 +0300

    refactor `OptimizationConfig`

--- autointent/_optimization_config.py
+++ autointent/_optimization_config.py
@@ -7,12 +7,25 @@ from .custom_types import SamplerType
 
 
 class OptimizationConfig(BaseModel):
-    """Configuration for the optimization process."""
+    """Configuration for the optimization process.
+    
+    One can use it to customize optimization beyond choosing different preset.
+    Instantiate it and pass to :py:meth:`autointent.Pipeline.from_optimization_config`.
+    """
 
     data_config: DataConfig = DataConfig()
+    
     search_space: list[dict[str, Any]]
+    """See tutorial on search space customization."""
+    
     logging_config: LoggingConfig = LoggingConfig()
+    """See tutorial on logging configuration."""
+    
     embedder_config: EmbedderConfig = EmbedderConfig()
+    
     cross_encoder_config: CrossEncoderConfig = CrossEncoderConfig()
+    
     sampler: SamplerType = "brute"
+    """See tutorial on optuna and presets."""
+    
     seed: PositiveInt = 42
