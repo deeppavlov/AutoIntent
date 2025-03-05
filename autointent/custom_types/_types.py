5c42d30a9585932e2dad4f9713f8519a73cbabe6
voorhs <ilya_alekseev_2016@list.ru>
2025-03-05 12:31:28 +0300

    minor fix

--- autointent/custom_types/_types.py
+++ autointent/custom_types/_types.py
@@ -119,10 +119,10 @@ SearchSpaceValidationMode = Literal["raise", "warning", "filter"]
 SearchSpacePreset = Literal["light", "light_moderate", "light_extra", "heavy", "heavy_moderate", "heavy_extra"]
 """Some presets that our library supports from lightest to heaviest:
 
-- `light_extra`: just like `light` but tuned with `optuna.samplers.RandomSampler`
-- `light`: only light-weight modules tuned with `optuna.samplers.TPESampler`
-- `light_moderate`: just like `light` but tuned with `optuna.samplers.BruteForceSampler`
-- `heavy_moderate`: just like `heavy` but tuned with `optuna.samplers.RandomSampler`
-- `light`: light-weight modules accompanied with heavy-weight ones tuned with `optuna.samplers.TPESampler`
-- `heavy_extra`: just like `light` but tuned with `optuna.samplers.BruteForceSampler`
+- `light_extra`: just like `light` but tuned with :py:class:`optuna.samplers.RandomSampler`
+- `light`: only light-weight modules tuned with :py:class:`optuna.samplers.TPESampler`
+- `light_moderate`: just like `light` but tuned with :py:class:`optuna.samplers.BruteForceSampler`
+- `heavy_moderate`: just like `heavy` but tuned with :py:class:`optuna.samplers.RandomSampler`
+- `light`: light-weight modules accompanied with heavy-weight ones tuned with :py:class:`optuna.samplers.TPESampler`
+- `heavy_extra`: just like `light` but tuned with :py:class:`optuna.samplers.BruteForceSampler`
 """
