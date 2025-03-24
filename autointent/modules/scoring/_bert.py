01bd0515388abd291f114eba33927126d412a91d
Алексеев Илья <44509110+voorhs@users.noreply.github.com>
2025-03-24 12:18:08 +0300

    Fix/docs building for bert scorer (#171)
    * fix
    
    * fix codestyle

--- autointent/modules/scoring/_bert.py
+++ autointent/modules/scoring/_bert.py
@@ -129,7 +129,7 @@ class BertScorer(BaseScorer):
 
         all_predictions = []
         for i in range(0, len(utterances), self.batch_size):
-            batch = utterances[i:i + self.batch_size]
+            batch = utterances[i : i + self.batch_size]
             inputs = self._tokenizer(batch, return_tensors="pt", **self.model_config.tokenizer_config.model_dump())
             with torch.no_grad():
                 outputs = self._model(**inputs)
