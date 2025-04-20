2f863d39c960c88ca3b67400a7ba1f6e288a9ced
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-04-20 18:09:17 +0300

    fix test

--- tests/data/test_data_handler.py
+++ tests/data/test_data_handler.py
@@ -233,10 +233,10 @@ def test_few_shot_split(dataset):
     desired_specs = {
         "train_0": {0: 2, 1: 2, 2: 2, 3: 2},
         "train_1": {2: 2, 0: 2, None: 2, 1: 1, 3: 1},
-        "validation_0": {1: 1, 3: 1, 0: 1, 2: 1},
-        "validation_1": {None: 4, 3: 1, 0: 1, 1: 1, 2: 1},
+        "validation_0": {0: 3, 1: 4, 2: 3, 3: 4, },
+        "validation_1": {None: 14, 3: 1, 0: 1, 1: 1, 2: 1},
         "test": {None: 4, 0: 2, 2: 2, 3: 2, 1: 2},
     }
 
     for data_split in dh.dataset:
-        assert Counter(dh.dataset[data_split][dh.dataset.label_feature]) == desired_specs[data_split]
+        assert Counter(dh.dataset[data_split][dh.dataset.label_feature]) == desired_specs[data_split], f"Failed for {data_split}"
