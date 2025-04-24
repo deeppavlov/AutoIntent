889c2e59e69a8947db3fb441cbe74c4199cb2e14
riapush <leriya022@gmail.com>
2025-04-24 15:31:32 +0300

    Update test_lora.py

--- tests/modules/scoring/test_lora.py
+++ tests/modules/scoring/test_lora.py
@@ -14,7 +14,11 @@ def test_lora_scorer_dump_load(dataset):
     data_handler = DataHandler(dataset)
 
     # Create and train scorer
-    scorer_original = BERTLoRAScorer(classification_model_config="prajjwal1/bert-tiny", num_train_epochs=1, batch_size=8)
+    scorer_original = BERTLoRAScorer(
+        classification_model_config="prajjwal1/bert-tiny",
+        num_train_epochs=1,
+        batch_size=8
+        )
     scorer_original.fit(data_handler.train_utterances(0), data_handler.train_labels(0))
 
     # Test data
@@ -33,7 +37,11 @@ def test_lora_scorer_dump_load(dataset):
         scorer_original.dump(str(temp_dir_path))
 
         # Create a new scorer and load saved model
-        scorer_loaded = BERTLoRAScorer(classification_model_config="prajjwal1/bert-tiny", num_train_epochs=1, batch_size=8)
+        scorer_loaded = BERTLoRAScorer(
+            classification_model_config="prajjwal1/bert-tiny",
+            num_train_epochs=1,
+            batch_size=8
+            )
         scorer_loaded.load(str(temp_dir_path))
 
         # Verify model and tokenizer are loaded
