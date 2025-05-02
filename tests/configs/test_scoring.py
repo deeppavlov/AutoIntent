90ebc07b0f6633974e4d71adf41b716d59f328d6
voorhs <ilya_alekseev_2016@list.ru>
2025-05-02 16:16:49 +0300

    upd test

--- tests/configs/test_scoring.py
+++ tests/configs/test_scoring.py
@@ -44,7 +44,7 @@ def valid_scoring_config():
                 "embedder_config": ["sergeyzh/rubert-tiny-turbo"],
                 "k": [5],
                 "weights": ["distance"],
-                "rank_threshold_cutoff": [None, 3],
+                "use_crosencoder_scores": [True, False],
             },
             {
                 "module_name": "sklearn",
