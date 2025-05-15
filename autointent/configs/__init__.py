9c1b6d0a40677198f0b7bbf17f1935dc852ec9ea
riapush <leriya022@gmail.com>
2025-05-15 22:03:28 +0300

    Update __init__.py

--- autointent/configs/__init__.py
+++ autointent/configs/__init__.py
@@ -2,7 +2,7 @@
 
 from ._inference_node import InferenceNodeConfig
 from ._optimization import DataConfig, LoggingConfig
-from ._transformers import CrossEncoderConfig, EmbedderConfig, HFModelConfig, TaskTypeEnum, TokenizerConfig
+from ._transformers import CNNConfig, CrossEncoderConfig, EmbedderConfig, HFModelConfig, TaskTypeEnum, TokenizerConfig
 
 __all__ = [
     "CNNConfig",
