ff3cf2ed01fcc7ecb3a9d3d733fb7abfc5a4f6d5
voorhs <ilya_alekseev_2016@list.ru>
2025-06-08 22:33:13 +0300

    try to fix `"eval_f1" not found` error

--- autointent/modules/scoring/_bert.py
+++ autointent/modules/scoring/_bert.py
@@ -132,7 +132,7 @@ class BertScorer(BaseScorer):
 
         tokenized_dataset = dataset.map(tokenize_function, batched=True, batch_size=self.batch_size)
 
-        metric_name = "f1"
+        metric_name = "eval_f1"
 
         def compute_metrics(predictions: EvalPrediction) -> dict[str, float]:
             return {metric_name: scoring_f1(predictions.label_ids.tolist(), predictions.predictions.tolist())}  # type: ignore[union-attr]
