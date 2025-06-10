8887c13904af5c5eb627d3e8b5d4534727bc3a1c
voorhs <ilya_alekseev_2016@list.ru>
2025-06-10 23:00:47 +0300

    try to fix typing issue

--- autointent/_dump_tools.py
+++ autointent/_dump_tools.py
@@ -26,7 +26,7 @@ from autointent.schemas import TagsList
 ModuleSimpleAttributes = None | str | int | float | bool | list  # type: ignore[type-arg]
 
 ModuleAttributes: TypeAlias = (
-    ModuleSimpleAttributes | TagsList | np.ndarray | Embedder | VectorIndex | BaseEstimator | Ranker
+    ModuleSimpleAttributes | TagsList | np.ndarray[Any] | Embedder | VectorIndex | BaseEstimator | Ranker
 )
 
 logger = logging.getLogger(__name__)
