e78bbefd363e75b209323132515eb6a314bd6149
voorhs <ilya_alekseev_2016@list.ru>
2025-06-14 23:20:18 +0300

    add `_wrappers` submodule

---
+++ autointent/_wrappers/base_torch_module.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from torch import nn
+from typing_extensions import Self
+
+
+class BaseTorchModule(nn.Module, ABC):
+    @abstractmethod
+    def dump(self, path: Path) -> None:
+        """Dump torch module to disk.
+
+        This method encapsulates all the logic of dumping module's weights and
+        hyperparameters required for initialization from disk and inference.
+
+        Args:
+            path: path in file system
+        """
+
+    @abstractmethod
+    @classmethod
+    def load(cls, path: Path, device: str | None = None) -> Self:
+        """Load torch module from disk.
+
+        This method loads all weights and hyperparameters required for
+        initialization from disk and inference.
+
+        Args:
+            path: path in file system
+            device: torch notation for CPU, CUDA, MPS, etc.
+        """
