19b641974f0c10fea1ac867a75f35ef76695543f
voorhs <ilya_alekseev_2016@list.ru>
2025-06-27 13:56:32 +0300

    proper testing

--- tests/pipeline/test_presets.py
+++ tests/pipeline/test_presets.py
@@ -1,23 +1,43 @@
-from typing import get_args
+import os
 
 import pytest
 
 from autointent import Pipeline
-from autointent.configs import DataConfig, LoggingConfig
-from autointent.custom_types import SearchSpacePreset
+from autointent.configs import DataConfig, HPOConfig, LoggingConfig
 from tests.conftest import setup_environment
 
 
-@pytest.mark.parametrize("preset", get_args(SearchSpacePreset))
+@pytest.mark.parametrize("preset", [
+    "classic-heavy",
+    "classic-light",
+    "classic-medium",
+    "nn-heavy",
+    "nn-medium",
+    pytest.param(
+        "transformers-heavy",
+        marks=pytest.mark.transformers
+    ),
+    pytest.param(
+        "transformers-light",
+        marks=pytest.mark.transformers
+    ),
+    pytest.param(
+        "transformers-no-hpo",
+        marks=pytest.mark.transformers
+    ),
+    "zero-shot-openai",
+    "zero-shot-transformers",
+])
 def test_presets(dataset, preset):
     project_dir = setup_environment()
 
     pipeline_optimizer = Pipeline.from_preset(preset)
 
-    if preset in ["heavy"]:
-        return
+    if preset == "zero-shot-openai" and not (os.getenv("OPENAI_API_KEY") or os.getenv("OPENAI_MODEL_NAME")):
+        pytest.skip(reason="OpenAI API key or model name is missing.")
 
     pipeline_optimizer.set_config(LoggingConfig(project_dir=project_dir, dump_modules=True, clear_ram=True))
     pipeline_optimizer.set_config(DataConfig(scheme="ho"))
+    pipeline_optimizer.set_config(HPOConfig(timeout=60))  # limit budget time because we want tests to be fast
 
     pipeline_optimizer.fit(dataset, refit_after=False)
