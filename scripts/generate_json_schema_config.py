d6efa957c444ce3f3375a3ec1d0155ee01428c4b
voorhs <ilya_alekseev_2016@list.ru>
2025-08-19 08:16:32 +0300

    add logger calls to json schema script

--- scripts/generate_json_schema_config.py
+++ scripts/generate_json_schema_config.py
@@ -1,5 +1,9 @@
 import json
+import logging
 from pathlib import Path
+
+logging.basicConfig(level="DEBUG")
+logger = logging.getLogger(__name__)
 
 from autointent import OptimizationConfig
 from autointent.schemas.node_validation import OptimizationSearchSpaceConfig
@@ -7,11 +11,13 @@ from autointent.schemas.node_validation import OptimizationSearchSpaceConfig
 
 def generate_json_schema_optimizer_config() -> None:
     """Generate the JSON schema for the optimizer config."""
+    logger.debug(f"Generating json schema for OptimizationConfig...")
     schema = OptimizationConfig.model_json_schema()
     path = Path(__file__).parent.parent / "docs" / "optimizer_config.schema.json"
     with path.open("w") as f:
         json.dump(schema, f, indent=4)
 
+    logger.debug(f"Generating json schema for OptimizationSearchSpaceConfig...")
     schema = OptimizationSearchSpaceConfig.model_json_schema()
     path = Path(__file__).parent.parent / "docs" / "optimizer_search_space_config.schema.json"
     with path.open("w") as f:
